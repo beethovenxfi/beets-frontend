9f1c7c0e4f193d0b554090c76bc97637fc84407c
1saf <me@ansaf.net>
2022-05-06 01:52:36 +1000

    feature: path aliases

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias["~"] = path.join(__dirname, 'src');
+    return config;
+  }
 }
 
 module.exports = nextConfig
