a195a9dff4f5c0776a90331485165abcd4c0165d
1saf <me@ansaf.net>
2022-05-06 01:53:56 +1000

    update: experimenting with styles

--- components/trade/TradeCard.tsx
+++ components/trade/TradeCard.tsx
@@ -1,12 +1,20 @@
 import { Box } from "@chakra-ui/react";
 
 function TradeCard() {
-
-    return (
-        <Box bg="gray.900" width="full" shadow="lg" rounded="lg" padding="4">
-            Trade Card
-        </Box>
-    )
+  return (
+    <Box
+      bg="#0F0F22"
+      border="2px"
+      borderColor="beets.green.800"
+      width="full"
+      height="96"
+      shadow="2xl"
+      rounded="xl"
+      padding="4"
+    >
+      Trade Card
+    </Box>
+  );
 }
 
 export default TradeCard;
