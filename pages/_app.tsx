94e8a30784e7d1346afef695b9a5183d7cb233cc
1saf <me@ansaf.net>
2022-05-06 01:53:00 +1000

    feature: theme extension and navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, extendTheme, VStack } from "@chakra-ui/react";
 
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
+import FantomTheme from "~/styles/themes/fantom.json";
+import Navbar from "~/components/nav/Navbar";
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = extendTheme(FantomTheme);
   return (
-    <ChakraProvider>
-      <Box height="full">
-        <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Box height="full" className="bg">
+        <Box height="full" className="bg-gradient">
+          <VStack width="full">
+            <Navbar />
+            <Component {...pageProps} />
+          </VStack>
+        </Box>
       </Box>
     </ChakraProvider>
   );
