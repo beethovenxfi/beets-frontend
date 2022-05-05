3f8e4428585eac32794dd90585396139adbcc927
1saf <me@ansaf.net>
2022-05-06 01:58:53 +1000

    update: setup charting library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,75 @@
-import { Box, ChakraProvider, extendTheme, VStack } from "@chakra-ui/react";
-
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+
+import { Box, ChakraProvider, extendTheme, VStack } from "@chakra-ui/react";
+
+/** Start charting library setup */
+import ReactEChartsCore from "echarts-for-react/lib/core";
+import * as echarts from "echarts/core";
+
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  CandlestickChart,
+  LinesChart,
+} from "echarts/charts";
+
+import {
+  GridSimpleComponent,
+  GridComponent,
+  SingleAxisComponent,
+  GraphicComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  AxisPointerComponent,
+  BrushComponent,
+  TitleComponent,
+  TimelineComponent,
+  MarkPointComponent,
+  MarkLineComponent,
+  MarkAreaComponent,
+  LegendComponent,
+  LegendScrollComponent,
+  LegendPlainComponent,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+  DatasetComponent,
+} from "echarts/components";
+
+import { CanvasRenderer } from "echarts/renderers";
+
+echarts.use([
+  LineChart,
+  BarChart,
+  PieChart,
+  CandlestickChart,
+  LinesChart,
+  GridSimpleComponent,
+  GridComponent,
+  SingleAxisComponent,
+  GraphicComponent,
+  ToolboxComponent,
+  TooltipComponent,
+  AxisPointerComponent,
+  BrushComponent,
+  TitleComponent,
+  TimelineComponent,
+  MarkPointComponent,
+  MarkLineComponent,
+  MarkAreaComponent,
+  LegendComponent,
+  LegendScrollComponent,
+  LegendPlainComponent,
+  DataZoomComponent,
+  DataZoomInsideComponent,
+  DataZoomSliderComponent,
+  DatasetComponent,
+  CanvasRenderer,
+]);
+
+/** End charting library setup */
 
 import FantomTheme from "~/styles/themes/fantom.json";
 import Navbar from "~/components/nav/Navbar";
