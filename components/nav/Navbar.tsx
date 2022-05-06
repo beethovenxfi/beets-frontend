428ec2fb6875e036cefa92eae87dccb663656708
1saf <me@ansaf.net>
2022-05-06 21:56:14 +1000

    fix: return navbar

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,7 +1,14 @@
 import { Box } from '@chakra-ui/react';
+import Image from 'next/image';
+
+import LogoFull from '~/assets/logo/logo-full.svg';
 
 function Navbar() {
-    return <Box width="full"></Box>;
+    return (
+        <Box width="full" padding="4">
+            <Image src={LogoFull} alt="Logo" />
+        </Box>
+    );
 }
 
 export default Navbar;
