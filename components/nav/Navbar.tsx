9a6ae1b9243b86e42168cffb91d7fcb1be870a13
1saf <me@ansaf.net>
2022-05-09 02:25:43 +1000

    chore: cleanup

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,13 +1,13 @@
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import Image from 'next/image';
 
 import LogoFull from '~/assets/logo/logo-full.svg';
 
 function Navbar() {
     return (
-        <Box width="full" padding="4">
+        <Flex width="full" padding="4">
             <Image src={LogoFull} alt="Logo" />
-        </Box>
+        </Flex>
     );
 }
 
