e88362a2b2fed44d48e34c1e1ba0c2bc4359bc12
Daniel <danielBeets@protonmail.com>
2022-05-13 12:00:24 +0200

    set the color mode to dark

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+import { chakraTheme } from '~/styles/chakraTheme';
 
 class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
+                    <ColorModeScript initialColorMode={chakraTheme.config.initialColorMode} />
                     <Main />
                     <NextScript />
                 </body>
