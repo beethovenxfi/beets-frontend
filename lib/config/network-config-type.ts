c04806fc48c441a438e1a04d1efc913a2d56832f
Daniel <danielBeets@protonmail.com>
2022-05-17 08:39:28 +0200

    initial network config definition

---
+++ lib/config/network-config-type.ts
@@ -0,0 +1,32 @@
+export interface NetworkConfig {
+    chainId: string;
+    eth: {
+        address: string;
+        symbol: string;
+        decimals: number;
+    };
+    wethAddress: string;
+    wethAddressFormatted: string;
+    rpcUrl: string;
+    coingecko: {
+        nativeAssetId: string;
+        platformId: string;
+    };
+    multicall: string;
+    /*subgraphs: {
+        balancer: string;
+        blocks: string;
+        masterchef: string;
+        beetsBar: string;
+        changelog: string;
+        locking: string;
+    };*/
+    beets: {
+        address: string;
+    };
+    fbeets: {
+        address: string;
+        farmId: string;
+        poolId: string;
+    };
+}
