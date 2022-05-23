0aa845a7b24399f0f44d3f809ee75b28e8c97857
Daniel <danielBeets@protonmail.com>
2022-05-23 17:20:28 +0200

    remove unnecessary props

--- modules/pool/invest/PoolInvest.tsx
+++ modules/pool/invest/PoolInvest.tsx
@@ -1,8 +1,6 @@
-import { Box, Container, Flex, Heading } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
 import { usePool } from '~/modules/pool/lib/usePool';
-import { useUserBalances } from '~/lib/global/useUserBalances';
 import PoolTokensInWallet from '~/modules/pool/components/PoolTokensInWallet';
-import { tokenGetAmountForAddress } from '~/lib/services/token/token-util';
 import PoolMyPoolBalance from '~/modules/pool/components/PoolMyPoolBalance';
 import PoolInvestForm from '~/modules/pool/invest/components/PoolInvestForm';
 import { usePoolUserBalances } from '~/modules/pool/lib/usePoolUserBalances';
@@ -16,7 +14,7 @@ function PoolInvest() {
             <Flex alignItems="flex-start">
                 <PoolTokensInWallet />
                 <PoolInvestForm pool={pool} mx={8} flex={1} userBalances={userBalances} />
-                <PoolMyPoolBalance pool={pool} userBptBalance={userBptBalance} />
+                <PoolMyPoolBalance />
             </Flex>
         </Container>
     );
