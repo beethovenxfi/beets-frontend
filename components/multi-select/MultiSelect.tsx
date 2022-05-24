b6ff3b6516958035d87f27f955f36ef00c6b2305
Daniel <danielBeets@protonmail.com>
2022-05-24 18:56:20 +0200

    create generic multi select component

---
+++ components/multi-select/MultiSelect.tsx
@@ -0,0 +1,68 @@
+import { Box } from '@chakra-ui/react';
+import { chakraComponents, ChakraStylesConfig, Select } from 'chakra-react-select';
+import React from 'react';
+
+const chakraStyles: ChakraStylesConfig = {
+    dropdownIndicator: (provided, state) => ({
+        ...provided,
+        background: 'rgba(255,255,255,0.08)',
+        p: 0,
+        w: '40px',
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        background: state.isFocused ? 'beets.base.800' : 'transparent',
+        //background: 'beets.base.800',
+    }),
+    menuList: (provided, state) => ({
+        ...provided,
+        //background: state.isFocused ? 'blue.100' : 'black',
+        background: 'beets.base.700',
+    }),
+    multiValue: (provided, state) => ({
+        ...provided,
+        background: 'beets.box.500',
+    }),
+    multiValueLabel: (provided, state) => ({
+        ...provided,
+        color: 'white',
+    }),
+    multiValueRemove: (provided, state) => ({
+        ...provided,
+        color: 'white',
+    }),
+};
+
+interface Props<T> {
+    placeholder?: string;
+    options: T[];
+    renderOption: (data: T, children: any) => React.ReactNode;
+    renderMultiValue: (data: T, children: any) => React.ReactNode;
+}
+
+export function MultiSelect<T>({ placeholder, options, renderOption, renderMultiValue }: Props<T>) {
+    const customComponents = {
+        Option: ({ children, ...props }: any) => (
+            <chakraComponents.Option {...props}>{renderOption(props.data, children)}</chakraComponents.Option>
+        ),
+        MultiValue: ({ children, ...props }: any) => {
+            return (
+                <chakraComponents.MultiValue {...props}>
+                    {renderMultiValue(props.data, children)}
+                </chakraComponents.MultiValue>
+            );
+        },
+    };
+
+    return (
+        <Box>
+            <Select
+                options={options}
+                isMulti
+                chakraStyles={chakraStyles}
+                placeholder={placeholder}
+                components={customComponents}
+            />
+        </Box>
+    );
+}
--- modules/pools/components/PoolListTokenMultiSelect.tsx
+++ modules/pools/components/PoolListTokenMultiSelect.tsx
@@ -1,75 +1,32 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react';
-import { chakraComponents, ChakraStylesConfig, Select } from 'chakra-react-select';
 import { useGetTokens } from '~/lib/global/useToken';
 import TokenAvatar from '~/components/token/TokenAvatar';
-
-const chakraStyles: ChakraStylesConfig = {
-    dropdownIndicator: (provided, state) => ({
-        ...provided,
-        background: 'rgba(255,255,255,0.08)',
-        p: 0,
-        w: '40px',
-    }),
-    option: (provided, state) => ({
-        ...provided,
-        background: state.isFocused ? 'beets.base.800' : 'transparent',
-        //background: 'beets.base.800',
-    }),
-    menuList: (provided, state) => ({
-        ...provided,
-        //background: state.isFocused ? 'blue.100' : 'black',
-        background: 'beets.base.700',
-    }),
-    multiValue: (provided, state) => ({
-        ...provided,
-        background: 'beets.box.500',
-    }),
-    multiValueLabel: (provided, state) => ({
-        ...provided,
-        color: 'white',
-    }),
-    multiValueRemove: (provided, state) => ({
-        ...provided,
-        color: 'white',
-    }),
-};
+import { MultiSelect } from '~/components/multi-select/MultiSelect';
 
 export function PoolListTokenMultiSelect() {
     const { tokens } = useGetTokens();
 
-    const customComponents = {
-        Option: ({ children, ...props }: any) => (
-            <chakraComponents.Option {...props}>
-                {props.data.icon} <Text fontSize="lg">{props.data.label}</Text>
-            </chakraComponents.Option>
-        ),
-        MultiValue: ({ children, ...props }: any) => {
-            return (
-                <chakraComponents.MultiValue {...props}>
-                    <Flex alignItems="center">
-                        {props.data.imageUrl ? (
-                            <Image src={props.data.imageUrl} style={{ width: 20, height: 20 }} mr={1} />
-                        ) : null}
-                        {children}
-                    </Flex>
-                </chakraComponents.MultiValue>
-            );
-        },
-    };
-
     return (
         <Box>
-            <Select
+            <MultiSelect
                 options={tokens.map((token) => ({
                     value: token.address,
                     label: token.symbol,
                     imageUrl: token.logoURI,
                     icon: <TokenAvatar address={token.address} size="xs" mr={2} />,
                 }))}
-                isMulti
-                chakraStyles={chakraStyles}
+                renderOption={(data, children) => (
+                    <>
+                        {data.icon} <Text fontSize="lg">{data.label}</Text>
+                    </>
+                )}
+                renderMultiValue={(data, children) => (
+                    <Flex alignItems="center">
+                        {data.imageUrl ? <Image src={data.imageUrl} style={{ width: 20, height: 20 }} mr={1} /> : null}
+                        {children}
+                    </Flex>
+                )}
                 placeholder="Filter by token..."
-                components={customComponents}
             />
         </Box>
     );
