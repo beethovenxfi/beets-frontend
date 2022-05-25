79754fcaed81248d7d26671f3a139491c83144a2
Daniel <danielBeets@protonmail.com>
2022-05-25 17:16:35 +0200

    fix type error

--- components/inputs/TokenInput.tsx
+++ components/inputs/TokenInput.tsx
@@ -13,11 +13,17 @@ type Props = {
 };
 
 export default function TokenInput({ label, toggleTokenSelect, address, onChange, value }: Props) {
-const { getToken } = useGetTokens();
+    const { getToken } = useGetTokens();
     return (
         <VStack width="full" alignItems="flex-start">
             <Box position="relative" width="full">
-                <BeetsInput value={value} onChange={onChange} placeholder="0" type="number" label={label} />
+                <BeetsInput
+                    value={value || undefined}
+                    onChange={onChange}
+                    placeholder="0"
+                    type="number"
+                    label={label}
+                />
                 <Box position="absolute" zIndex="toast" right=".75rem" top="50%" transform="translateY(-50%)">
                     <Button
                         onClick={toggleTokenSelect}
