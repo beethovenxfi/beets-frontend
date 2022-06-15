01ae01bdbbcb9fb3860abc4808da8d700fda8c83
Daniel <danielBeets@protonmail.com>
2022-06-15 13:16:28 +0800

    add indicator when there is at least one pool filter selected

--- modules/pools/components/PoolListTop.tsx
+++ modules/pools/components/PoolListTop.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, IconButton } from '@chakra-ui/react';
+import { Box, Circle, Flex, IconButton } from '@chakra-ui/react';
 import { PoolListTabs } from '~/modules/pools/components/PoolListTabs';
 import { PoolListSearch } from '~/modules/pools/components/PoolListSearch';
 import { Filter } from 'react-feather';
@@ -10,22 +10,28 @@ import { usePoolList } from '~/modules/pools/usePoolList';
 import { FadeInOutBox } from '~/components/animation/FadeInOutBox';
 
 export function PoolListTop() {
-    const { showFilters, toggleFilterVisibility } = usePoolList();
+    const { showFilters, toggleFilterVisibility, state } = usePoolList();
+    const hasFiltersSelected = (state.where?.filterIn || []).length > 0 || (state.where?.tokensIn || []).length > 0;
 
     return (
         <Box>
             <Flex pb={4}>
                 <Flex flex={1}>
                     <PoolListTabs />
-                    <IconButton
-                        aria-label="filter-button"
-                        icon={<Filter />}
-                        ml={4}
-                        onClick={toggleFilterVisibility}
-                        color={showFilters ? 'gray.100' : 'white'}
-                        bgColor={showFilters ? 'beets.base.300' : 'beets.lightAlpha.200'}
-                        _hover={{ bgColor: 'beets.light' }}
-                    />
+                    <Box position="relative">
+                        <IconButton
+                            aria-label="filter-button"
+                            icon={<Filter />}
+                            ml={4}
+                            onClick={toggleFilterVisibility}
+                            color={showFilters ? 'gray.100' : 'white'}
+                            bgColor={showFilters ? 'beets.base.300' : 'beets.lightAlpha.200'}
+                            _hover={{ bgColor: 'beets.light' }}
+                        />
+                        {hasFiltersSelected ? (
+                            <Circle size="3" bg="red.500" opacity="0.85" position="absolute" top="-4px" right="-4px" />
+                        ) : null}
+                    </Box>
                 </Flex>
                 <Box>
                     <PoolListSearch />
