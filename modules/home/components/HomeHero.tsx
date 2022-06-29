520a435dd30ecafa8fb2f9e7d46007799475f6a0
Daniel <danielBeets@protonmail.com>
2022-06-29 13:41:15 +0800

    hero mobile

--- modules/home/components/HomeHero.tsx
+++ modules/home/components/HomeHero.tsx
@@ -12,7 +12,7 @@ export function HomeHero() {
             minHeight="400px"
             backgroundImage={{
                 base: "url('/images/hero-image-fantom-mobile.png')",
-                xl: "url('/images/hero-image-fantom.png')",
+                md: "url('/images/hero-image-fantom.png')",
             }}
             backgroundPosition="center"
             backgroundRepeat="no-repeat"
@@ -20,20 +20,26 @@ export function HomeHero() {
             boxShadow="0px 0px 24px 0px rgba(0,0,0,0.25);"
         >
             <Flex flex="1" mt="20" pl={{ base: '4', xl: '8' }}>
-                <Flex flexDirection="column" width="580px">
-                    <Text as="h1" textStyle="h1" textTransform="uppercase" color="white" fontWeight="semibold">
+                <Flex flexDirection="column" width={{ base: 'auto', lg: '580px' }}>
+                    <Text
+                        as="h1"
+                        textStyle={{ base: 'h2', lg: 'h1' }}
+                        textTransform="uppercase"
+                        color="white"
+                        fontWeight="semibold"
+                    >
                         Welcome to
                         <br />
                         Beethoven X
                     </Text>
-                    <Text color="white" as="h5" textStyle="h5" mt="10" mb="10">
+                    <Text color="white" as="h5" textStyle={{ base: 'lg', lg: 'h5' }} mt="10" mb="10">
                         Built for traders, investors and protocols, we are your complete automated portfolio manager and
                         trading solution.
                     </Text>
 
                     <HStack spacing="4" mb="10">
-                        <BeetsButton width="160px">Invest</BeetsButton>
-                        <BeetsButton width="160px" buttonType="secondary">
+                        <BeetsButton width={{ base: '130px', lg: '160px' }}>Invest</BeetsButton>
+                        <BeetsButton width={{ base: '130px', lg: '160px' }} buttonType="secondary">
                             Swap
                         </BeetsButton>
                     </HStack>
@@ -42,7 +48,7 @@ export function HomeHero() {
                     </Link>
                 </Flex>
             </Flex>
-            <Box flex="1" />
+            <Box flex="1" display={{ base: 'none', md: 'block' }} />
         </Flex>
     );
 }
