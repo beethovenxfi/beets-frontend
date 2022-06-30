c47e0641295935a6502edbefcb2e4bc61001a03c
groninge <groninge@hotmail.com>
2022-06-30 17:05:26 +0200

    add token pop over

--- modules/pools/components/PoolListItem.tsx
+++ modules/pools/components/PoolListItem.tsx
@@ -1,5 +1,5 @@
 import { GqlPoolMinimalFragment } from '~/apollo/generated/graphql-codegen-generated';
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text, Popover, PopoverTrigger as OrigPopoverTrigger, PopoverContent } from '@chakra-ui/react';
 import TokenAvatarSet from '~/components/token/TokenAvatarSet';
 import Link from 'next/link';
 import numeral from 'numeral';
@@ -7,6 +7,8 @@ import AprTooltip from '~/components/apr-tooltip/AprTooltip';
 import { BoxProps } from '@chakra-ui/layout';
 import { AmountHumanReadable } from '~/lib/services/token/token-types';
 import { numberFormatUSDValue } from '~/lib/util/number-formats';
+import { PoolTokenPill } from '~/components/token/PoolTokenPill';
+import { useGetTokens } from '~/lib/global/useToken';
 
 interface Props extends BoxProps {
     pool: GqlPoolMinimalFragment;
@@ -15,19 +17,42 @@ interface Props extends BoxProps {
 }
 
 export default function PoolListItem({ pool, userBalance, showUserBalance, ...rest }: Props) {
+    // temp fix: https://github.com/chakra-ui/chakra-ui/issues/5896#issuecomment-1104085557
+    const PopoverTrigger: React.FC<{ children: React.ReactNode }> = OrigPopoverTrigger;
+
+    const { getToken } = useGetTokens();
+
+    const tokens = pool.allTokens
+        .filter((token) => !token.isNested && !token.isPhantomBpt)
+        .map((token) => {
+            const tokenData = getToken(token.address);
+            return { address: token.address, weight: token.weight, symbol: tokenData?.symbol };
+        });
+
     return (
         <Box {...rest}>
             <Link href={`/pool/${pool.id}`} passHref>
                 <a>
                     <Flex px="4" py="4" cursor="pointer" alignItems={'center'} fontSize="lg" _hover={{ bg: '#100C3A' }}>
                         <Box w={90} textAlign={'center'}>
-                            <TokenAvatarSet
-                                imageSize={25}
-                                width={92}
-                                addresses={pool.allTokens
-                                    .filter((token) => !token.isNested && !token.isPhantomBpt)
-                                    .map((token) => token.address)}
-                            />
+                            <Popover trigger="hover">
+                                <PopoverTrigger>
+                                    <a>
+                                        <TokenAvatarSet
+                                            imageSize={25}
+                                            width={92}
+                                            addresses={pool.allTokens
+                                                .filter((token) => !token.isNested && !token.isPhantomBpt)
+                                                .map((token) => token.address)}
+                                        />
+                                    </a>
+                                </PopoverTrigger>
+                                <PopoverContent w="fit-content" bgColor="beets.base.800" shadow="2xl">
+                                    {tokens.map((token, index) => (
+                                        <PoolTokenPill key={index} token={token} rounded={false}></PoolTokenPill>
+                                    ))}
+                                </PopoverContent>
+                            </Popover>
                         </Box>
                         <Flex flex={1}>
                             <Text fontSize="md">{pool.name}</Text>
