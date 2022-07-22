e733c02f71f615c9e7419e25ddd7a9ac975048c5
groninge <groninge@hotmail.com>
2022-07-22 11:30:52 +0200

    add volume/liquidity chart

--- modules/pool/detail/components/PoolDetailChart.tsx
+++ modules/pool/detail/components/PoolDetailChart.tsx
@@ -4,6 +4,8 @@ import Card from '~/components/card/Card';
 import ReactECharts from 'echarts-for-react';
 import { useMemo } from 'react';
 import { EChartsOption } from 'echarts';
+import numeral from 'numeral';
+import dateFormat from 'dateformat';
 
 interface Props extends BoxProps {}
 
@@ -12,7 +14,7 @@ function PoolDetailChart({ ...rest }: Props) {
     const option = useMemo<EChartsOption>(
         () => ({
             tooltip: {
-                show: false,
+                show: true,
                 // trigger: 'axis',
                 // type: 'shadow',
                 // backgroundColor: 'rgba(24, 24, 46, 0.95)',
@@ -31,50 +33,166 @@ function PoolDetailChart({ ...rest }: Props) {
                 //         opacity: 1,
                 //     },
                 // },
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'cross',
+                    crossStyle: {
+                        color: '#999',
+                    },
+                },
+            },
+            legend: {
+                data: ['Daily Volume', 'Liquidity'],
+                textStyle: {
+                    color: 'white',
+                },
             },
             grid: {
-                bottom: '10%',
+                bottom: '5%',
                 right: '5%',
                 left: '5%',
-                top: '4%',
+                top: '10%',
+                containLabel: true,
             },
             xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'Mon', 'Tue', 'Wed'],
+                type: 'time',
                 minorSplitLine: {
                     show: false,
                 },
                 axisTick: {
                     show: false,
                 },
-                axisLine: {
-                    lineStyle: {
-                        color: theme.colors.beets.base['100'],
+                // axisLine: {
+                //     lineStyle: {
+                //         color: theme.colors.beets.base['100'],
+                //     },
+                // },
+                axisLabel: {
+                    formatter: function (value: number) {
+                        return dateFormat(new Date(value), 'mmmm d');
+                    },
+                    rotate: 30,
+                },
+                maxInterval: 3600 * 1000 * 24,
+                axisPointer: {
+                    type: 'line',
+                    label: {
+                        formatter: function (params) {
+                            return dateFormat(new Date(params.value), 'mmmm d');
+                        },
                     },
                 },
             },
-            yAxis: {
-                type: 'value',
-                axisLine: {
-                    show: true,
-                    lineStyle: {
-                        color: theme.colors.beets.base['100'],
+            yAxis: [
+                {
+                    type: 'value',
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            color: theme.colors.beets.base['100'],
+                        },
+                    },
+                    minorSplitLine: {
+                        show: false,
+                    },
+                    splitLine: {
+                        show: false,
+                    },
+                    axisLabel: {
+                        formatter: function (value: number) {
+                            return `$${numeral(value).format('0a')}`;
+                        },
+                    },
+                    axisPointer: {
+                        label: {
+                            formatter: function (params) {
+                                return `$${numeral(params.value).format('0a')}`;
+                            },
+                        },
                     },
                 },
-                minorSplitLine: {
-                    show: false,
+                {
+                    type: 'value',
+                    max: 6000000, // align with left_axis
+                    axisLine: {
+                        show: true,
+                        lineStyle: {
+                            color: theme.colors.beets.base['100'],
+                        },
+                    },
+                    minorSplitLine: {
+                        show: false,
+                    },
+                    splitLine: {
+                        show: false,
+                    },
+                    axisLabel: {
+                        formatter: function (value: number) {
+                            return `$${numeral(value).format('0a')}`;
+                        },
+                    },
+                    axisPointer: {
+                        label: {
+                            formatter: function (params) {
+                                return `$${numeral(params.value).format('0a')}`;
+                            },
+                        },
+                    },
                 },
-                splitLine: {
-                    show: false,
-                },
-            },
+            ],
             color: ['rgba(0,255,255, 1.0)'],
             series: [
                 {
-                    data: [120, 200, 150, 80, 70, 110, 130, 200, 500, 50, 150],
+                    data: [
+                        [1656633600000, 51000000],
+                        [1656720000000, 52000000],
+                        [1656806400000, 59000000],
+                        [1656892800000, 58000000],
+                        [1656979200000, 54000000],
+                        [1657065600000, 55000000],
+                        [1657152000000, 57000000],
+                        [1657238400000, 53000000],
+                        [1657324800000, 51000000],
+                        [1657411200000, 59000000],
+                        [1657497600000, 58000000],
+                        [1657584000000, 55000000],
+                        [1657670400000, 52000000],
+                    ],
+                    name: 'Liquidity',
                     type: 'bar',
                     itemStyle: {
                         borderRadius: [5, 5, 0, 0],
+                    },
+                    tooltip: {
+                        valueFormatter: function (value) {
+                            return `$${numeral(value).format('0a')}`;
+                        },
+                    },
+                },
+                {
+                    data: [
+                        [1656633600000, 4100000],
+                        [1656720000000, 4200000],
+                        [1656806400000, 4900000],
+                        [1656892800000, 4800000],
+                        [1656979200000, 4400000],
+                        [1657065600000, 4500000],
+                        [1657152000000, 4700000],
+                        [1657238400000, 4300000],
+                        [1657324800000, 4100000],
+                        [1657411200000, 4900000],
+                        [1657497600000, 4800000],
+                        [1657584000000, 4500000],
+                        [1657670400000, 4200000],
+                    ],
+                    name: 'Daily Volume',
+                    type: 'line',
+                    yAxisIndex: 1,
+                    color: 'black',
+                    tooltip: {
+                        valueFormatter: function (value) {
+                            return `$${numeral(value).format('0a')}`;
+                        },
                     },
                 },
             ],
@@ -94,7 +212,7 @@ function PoolDetailChart({ ...rest }: Props) {
                     <ReactECharts option={option} style={{ height: '100%' }} />
                 </Box>
             </VStack>
-    </Card>
+        </Card>
     );
 }
 
