777c1bec8f7b9643a9576f3a05b3839835e3877f
mobiusTripper-crypto <99622829+mobiusTripper-crypto@users.noreply.github.com>
2022-07-29 12:19:43 -0400

    high price impact warning for invest

--- modules/pool/invest/lib/usePoolJoinGetBptOutAndPriceImpactForTokensIn.ts
+++ modules/pool/invest/lib/usePoolJoinGetBptOutAndPriceImpactForTokensIn.ts
@@ -4,6 +4,8 @@ import { useReactiveVar } from '@apollo/client';
 import { investStateVar } from '~/modules/pool/invest/lib/useInvestState';
 import { tokenAmountsGetArrayFromMap } from '~/lib/services/token/token-util';
 import { useSlippage } from '~/lib/global/useSlippage';
+import { HIGH_PRICE_IMPACT, MEDIUM_PRICE_IMPACT } from '~/modules/constants';
+import numeral from 'numeral';
 
 export function usePoolJoinGetBptOutAndPriceImpactForTokensIn() {
     const { poolService } = usePool();
@@ -11,11 +13,25 @@ export function usePoolJoinGetBptOutAndPriceImpactForTokensIn() {
     const { slippage } = useSlippage();
     const tokenAmountsIn = tokenAmountsGetArrayFromMap(inputAmounts);
 
-    return useQuery(
+    const { data: bptOutAndPriceImpact, isLoading } = useQuery(
         ['joinGetBptOutAndPriceImpactForTokensIn', tokenAmountsIn, slippage],
         () => {
             return poolService.joinGetBptOutAndPriceImpactForTokensIn(tokenAmountsIn, slippage);
         },
         { enabled: tokenAmountsIn.length > 0, staleTime: 0, cacheTime: 0 },
     );
+
+    const hasHighPriceImpact = Math.abs(bptOutAndPriceImpact?.priceImpact || 0) > HIGH_PRICE_IMPACT;
+    const hasMediumPriceImpact =
+        !hasHighPriceImpact && Math.abs(bptOutAndPriceImpact?.priceImpact || 0) > MEDIUM_PRICE_IMPACT;
+
+    const formattedPriceImpact = numeral(Math.abs(bptOutAndPriceImpact?.priceImpact || 0)).format('0.00%');
+
+    return {
+        bptOutAndPriceImpact,
+        hasHighPriceImpact,
+        hasMediumPriceImpact,
+        formattedPriceImpact,
+        isLoading,
+    };
 }
