34884b1a4057e3297b877da027203f7f1a10f8cf
groninge <groninge@hotmail.com>
2022-07-31 12:19:13 +0200

    adjust for mobile

--- modules/pool/detail/components/transactions/PoolTransactionsHeader.tsx
+++ modules/pool/detail/components/transactions/PoolTransactionsHeader.tsx
@@ -1,38 +1,48 @@
-import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Box, Flex, Text } from '@chakra-ui/layout';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 export default function PoolTransactionHeader() {
     return (
-        <Flex
+        <Grid
             px="4"
-            py="3"
-            cursor="pointer"
+            py={{ base: '4', xl: '2' }}
             borderTopLeftRadius="md"
             borderTopRightRadius="md"
-            alignItems={'center'}
+            alignItems="center"
             bgColor="rgba(255,255,255,0.08)"
             borderBottom="2px"
             borderColor="beets.base.500"
+            mb={{ base: '4', lg: '0' }}
+            templateColumns={'200px 1fr 200px 200px'}
+            gap="0"
+            display={{ base: 'none', xl: 'grid' }}
         >
-            <Box width="200px">
+            <GridItem>
                 <Text fontSize="md" fontWeight="semibold" color="beets.base.100">
                     Action
                 </Text>
-            </Box>
-            <Box flex={1} textAlign="left">
+            </GridItem>
+            <GridItem>
                 <Text fontSize="md" fontWeight="semibold" color="beets.base.100">
                     Details
                 </Text>
-            </Box>
-            <Box width="200px" textAlign="right">
+            </GridItem>
+            <GridItem>
                 <Text fontSize="md" fontWeight="semibold" color="beets.base.100">
                     Value
                 </Text>
-            </Box>
-            <Box width="200px" textAlign="right">
-                <Text fontSize="md" fontWeight="semibold" color="beets.base.100">
+            </GridItem>
+            <GridItem>
+                <Text
+                    fontSize="md"
+                    fontWeight="semibold"
+                    color="beets.base.100"
+                    textAlign={{ base: 'left', lg: 'right' }}
+                    mr="6"
+                >
                     Time
                 </Text>
-            </Box>
-        </Flex>
+            </GridItem>
+        </Grid>
     );
 }
