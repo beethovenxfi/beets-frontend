05d89bbb45d3fdd90b35577299bf71221e9094b1
Daniel <danielBeets@protonmail.com>
2022-08-01 11:25:07 +0200

    fix spacing on chart loading state

--- modules/trade/components/TradeChart.tsx
+++ modules/trade/components/TradeChart.tsx
@@ -1,18 +1,14 @@
 import { TokenPriceLineChart } from '~/components/charts/TokenPriceLineChart';
 import { useTradeChart } from '~/modules/trade/lib/useTradeChart';
-import { NetworkStatus } from '@apollo/client';
-import { Box, Button, Flex, HStack, Link, Skeleton } from '@chakra-ui/react';
+import { Box, Flex, HStack, Link, Skeleton } from '@chakra-ui/react';
 import { useTradeData } from '~/modules/trade/lib/useTradeData';
-import { BeetsBox } from '~/components/box/BeetsBox';
 import { tokenFormatAmount } from '~/lib/services/token/token-util';
-
-import { cssVar } from '@chakra-ui/theme-tools';
 
 export function TradeChart() {
     const { setRange, range, data, loading, networkStatus } = useTradeChart();
     const { tokenOut, tokenIn } = useTradeData();
 
-    if (loading && !data) {
+    /*if (loading && !data) {
         //loading and no data
         return <Skeleton height="150px" />;
     }
@@ -28,24 +24,26 @@ export function TradeChart() {
     if (!tokenIn || !tokenOut) {
         //tokens not yet populated
         return <Skeleton height="150px" />;
-    }
+    }*/
 
     const sevenDaySelected = range === 'SEVEN_DAY';
     const thirtyDaySelected = range === 'THIRTY_DAY';
 
     return (
         <Box>
-            <Box height="150px">
-                <TokenPriceLineChart
-                    prices={data?.prices || []}
-                    label={``}
-                    priceValueFormatter={(value) =>
-                        `1 ${tokenOut?.symbol} = ${tokenFormatAmount(value)} ${tokenIn?.symbol}`
-                    }
-                />
-            </Box>
-            {/* <BeetsBox height="150px" />*/}
-            {/*<Skeleton height="3xs" />*/}
+            {(loading && !data) || !tokenIn || !tokenOut ? (
+                <Skeleton height="150px" />
+            ) : (
+                <Box height="150px">
+                    <TokenPriceLineChart
+                        prices={data?.prices || []}
+                        label={``}
+                        priceValueFormatter={(value) =>
+                            `1 ${tokenOut?.symbol} = ${tokenFormatAmount(value)} ${tokenIn?.symbol}`
+                        }
+                    />
+                </Box>
+            )}
             <Flex mt="2">
                 <Box flex={1} />
                 <HStack>
