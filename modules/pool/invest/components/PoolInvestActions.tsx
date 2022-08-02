61780e98832e1d4690fa293cd6b66c6c46185295
Daniel <danielBeets@protonmail.com>
2022-08-02 07:48:35 +0200

    clean up component

--- modules/pool/invest/components/PoolInvestActions.tsx
+++ modules/pool/invest/components/PoolInvestActions.tsx
@@ -1,43 +1,18 @@
-import { BoxProps } from '@chakra-ui/react';
 import { useUserAllowances } from '~/lib/util/useUserAllowances';
 import { usePool } from '~/modules/pool/lib/usePool';
 import { networkConfig } from '~/lib/config/network-config';
 import { useInvest } from '~/modules/pool/invest/lib/useInvest';
 import { useEffect, useState } from 'react';
-import { TokenBaseWithAmount } from '~/lib/services/token/token-types';
 import { usePoolJoinGetBptOutAndPriceImpactForTokensIn } from '~/modules/pool/invest/lib/usePoolJoinGetBptOutAndPriceImpactForTokensIn';
 import { usePoolJoinGetContractCallData } from '~/modules/pool/invest/lib/usePoolJoinGetContractCallData';
 import { useJoinPool } from '~/modules/pool/invest/lib/useJoinPool';
 import { BeetsTransactionStepsSubmit, TransactionStep } from '~/components/button/BeetsTransactionStepsSubmit';
 
-type Status = 'current' | 'idle' | 'submitting' | 'pending' | 'complete';
-type Step = TokenApprovalStep | ContractApprovalStep | InvestStep;
-
-interface BaseStep {
-    id: string;
-    buttonText: string;
-    tooltipText: string;
-}
-
-interface TokenApprovalStep extends BaseStep {
-    type: 'tokenApproval';
-    token: TokenBaseWithAmount;
-}
-
-interface ContractApprovalStep extends BaseStep {
-    type: 'contractApproval';
-    contractAddress: string;
-}
-
-interface InvestStep extends BaseStep {
-    type: 'invest';
-}
-
-interface Props extends BoxProps {
+interface Props {
     onInvestComplete(): void;
 }
 
-export function PoolInvestActions({ onInvestComplete, ...rest }: Props) {
+export function PoolInvestActions({ onInvestComplete }: Props) {
     const { pool } = usePool();
     const { selectedInvestTokensWithAmounts } = useInvest();
     const joinQuery = useJoinPool(pool);
@@ -76,78 +51,6 @@ export function PoolInvestActions({ onInvestComplete, ...rest }: Props) {
             setSteps(steps);
         }
     }, [isLoading]);
-
-    /*return (
-        <Box {...rest}>
-            {loading ? (
-                <Flex justifyContent="center">
-                    <Skeleton height="30px" width="180px" />
-                </Flex>
-            ) : steps && steps.length > 1 ? (
-                <HorizontalSteps
-                    mb="6"
-                    steps={(steps || []).map((step) => ({
-                        ...step,
-                        status: stepStatuses[step.id] || 'idle',
-                    }))}
-                />
-            ) : null}
-            {loading ? (
-                <Button isLoading={true} isFullWidth>
-                    Invest
-                </Button>
-            ) : null}
-            {steps && currentStep && currentStep.type === 'tokenApproval' ? (
-                <BeetsTokenApprovalButton
-                    tokenWithAmount={currentStep.token}
-                    onConfirmed={() => {
-                        setStepStatuses({
-                            ...stepStatuses,
-                            [currentStep.id]: 'complete',
-                            [steps[currentStepIdx + 1].id]: 'current',
-                        });
-
-                        refetchUserAllowances();
-                        setCurrentStepIdx(currentStepIdx + 1);
-                    }}
-                    onSubmitting={() => setStepStatus(currentStep.id, 'submitting')}
-                    onPending={() => setStepStatus(currentStep.id, 'pending')}
-                    onCanceled={() => setStepStatus(currentStep.id, 'current')}
-                />
-            ) : null}
-            {currentStep && currentStep.type === 'invest' && !isConfirmed ? (
-                <BeetsSubmitTransactionButton
-                    isFullWidth
-                    isSubmitting={isSubmitting}
-                    isPending={isPending}
-                    onClick={() => {
-                        if (contractCallData) {
-                            joinPool(contractCallData, selectedInvestTokensWithAmounts);
-                        }
-                    }}
-                >
-                    Invest
-                </BeetsSubmitTransactionButton>
-            ) : null}
-            {isConfirmed && (
-                <Button onClick={onInvestComplete} isFullWidth buttonType="secondary">
-                    Return to pool
-                </Button>
-            )}
-            {submitError ? (
-                <Alert status="error" mt={4}>
-                    <AlertIcon />
-                    An error occurred: {submitError.message}
-                </Alert>
-            ) : null}
-            {isConfirmed && (
-                <div className="fireworks">
-                    <div className="before" />
-                    <div className="after" />
-                </div>
-            )}
-        </Box>
-    );*/
 
     return (
         <BeetsTransactionStepsSubmit
