052a59fca3d19a2e69b8b67ad7b8626135bc2b16
Daniel <danielBeets@protonmail.com>
2022-08-04 12:27:52 +0200

    add static json rpc provider

--- lib/services/rpc-provider/rpc-provider.service.ts
+++ lib/services/rpc-provider/rpc-provider.service.ts
@@ -1,5 +1,6 @@
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { networkConfig } from '~/lib/config/network-config';
+import { StaticJsonRpcBatchProvider } from '~/lib/services/rpc-provider/static-json-rpc-batch-provier';
 
 type NewBlockHandler = (blockNumber: number) => any;
 
@@ -7,7 +8,7 @@ export class RpcProviderService {
     private readonly jsonProvider: JsonRpcProvider;
 
     constructor(private readonly rpcUrl: string) {
-        this.jsonProvider = new JsonRpcProvider(this.rpcUrl);
+        this.jsonProvider = new StaticJsonRpcBatchProvider(this.rpcUrl);
     }
 
     public initBlockListener(newBlockHandler: NewBlockHandler): void {
@@ -21,7 +22,7 @@ export class RpcProviderService {
     }
 
     public getJsonProvider(): JsonRpcProvider {
-        return new JsonRpcProvider(this.rpcUrl);
+        return new StaticJsonRpcBatchProvider(this.rpcUrl);
     }
 }
 
