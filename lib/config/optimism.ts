672586dec9d2a1211cfc58cbbc0edfcacb7f7b0e
Daniel <danielBeets@protonmail.com>
2022-08-04 11:04:28 +0200

    set pool owner address

--- lib/config/optimism.ts
+++ lib/config/optimism.ts
@@ -34,7 +34,7 @@ export const optimismNetworkConfig: NetworkConfig = {
         vault: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
         batchRelayer: '0x0000000000000000000000000000000000000000',
     },
-    beetsPoolOwnerAddress: '',
+    beetsPoolOwnerAddress: '0xd9e2889ac8c6fff8e94c7c1beeade1352df1a513',
     masterChefContractAddress: '',
     defaultTokenIn: '0x7F5c764cBc14f9669B88837ca1490cCa17c31607',
     defaultTokenOut: '0x4200000000000000000000000000000000000006',
