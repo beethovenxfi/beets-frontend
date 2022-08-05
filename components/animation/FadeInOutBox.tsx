b616efc277b66ebc821e58da239918a8a5183086
Daniel <danielBeets@protonmail.com>
2022-08-05 09:49:30 +0200

    remove import

--- components/animation/FadeInOutBox.tsx
+++ components/animation/FadeInOutBox.tsx
@@ -1,6 +1,5 @@
 import { Box, BoxProps } from '@chakra-ui/react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { CSSObject } from '@chakra-ui/styled-system/src/system.types';
 
 interface Props extends BoxProps {
     isVisible: boolean;
