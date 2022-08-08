55fcc47270d9e424ca3fe6c47af42789b394e998
Daniel <danielBeets@protonmail.com>
2022-08-08 11:41:46 +0200

    only new the provider when necessary

--- lib/services/nft/nft.service.ts
+++ lib/services/nft/nft.service.ts
@@ -3,9 +3,12 @@ import { BaseProvider } from '@ethersproject/providers';
 import { Contract } from 'ethers';
 import { StaticJsonRpcBatchProvider } from '~/lib/services/rpc-provider/static-json-rpc-batch-provier';
 import { fantomNetworkConfig } from '~/lib/config/fantom';
+import { networkConfig } from '~/lib/config/network-config';
+import { networkProvider } from '~/lib/global/network';
 
 export default class NftService {
-    constructor(private readonly contractAddress: string, private readonly provider: BaseProvider) {}
+    private _provider: BaseProvider | null = null;
+    constructor(private readonly contractAddress: string) {}
 
     public async balanceOf(user: string): Promise<string> {
         const contract = new Contract(this.contractAddress, BeethovenxNftAbi, this.provider);
@@ -27,9 +30,18 @@ export default class NftService {
 
         return result.toString();
     }
+
+    get provider(): BaseProvider {
+        if (this._provider === null) {
+            if (networkConfig.chainId === '250') {
+                this._provider = networkProvider;
+            } else {
+                this._provider = new StaticJsonRpcBatchProvider('https://rpc.ftm.tools');
+            }
+        }
+
+        return this._provider;
+    }
 }
 
-export const earlyLudwigNft = new NftService(
-    '0xf2558d6a252C1041a3902870B1FdB0B7524B3098',
-    new StaticJsonRpcBatchProvider('https://rpc.ftm.tools'),
-);
+export const earlyLudwigNft = new NftService('0xf2558d6a252C1041a3902870B1FdB0B7524B3098');
