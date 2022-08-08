4327d6982ea481bff06ef1f7cc6a12c24d3f0fd3
Daniel <danielBeets@protonmail.com>
2022-08-08 12:22:58 +0200

    hook up link

--- modules/home/components/HomeLearnItem.tsx
+++ modules/home/components/HomeLearnItem.tsx
@@ -11,7 +11,7 @@ export function HomeLearnItem({ title, description, url, last }: Props) {
     return (
         <Box borderBottomWidth={last ? 0 : 1} borderBottomColor="beets.base.500" mb="6" pb="6">
             <Box>
-                <Link fontSize="lg" fontWeight="semibold" mb="4">
+                <Link fontSize="lg" fontWeight="semibold" mb="4" href={url} target="_blank">
                     {title}
                 </Link>
                 <Box>{description}</Box>
