0eb5b24a4b23e392af36df94c42425885a99d6ed
groninge <groninge@hotmail.com>
2022-08-17 20:06:25 +0200

    update layout

--- modules/pool/og/PoolOpenGraph.tsx
+++ modules/pool/og/PoolOpenGraph.tsx
@@ -4,7 +4,10 @@ import { BeetsBox } from '~/components/box/BeetsBox';
 import TokenAvatar from '~/components/token/TokenAvatar';
 import { useGetTokens } from '~/lib/global/useToken';
 import { usePool } from '../lib/usePool';
-
+import Image from 'next/image';
+import InvestMastheadImage from '~/assets/images/invest-masthead-image.png';
+import InvestMastheadOpImage from '~/assets/images/invest-masthead-image-OP.png';
+import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
 interface Token {
     address: string;
     weight?: string | null;
@@ -19,7 +22,7 @@ export function PoolTokenPill({ token }: Props) {
     const { getToken } = useGetTokens();
 
     return (
-        <BeetsBox p="2" width="250px">
+        <BeetsBox p="2" width="265px">
             <Flex alignItems="center" justifyContent="space-evenly">
                 <TokenAvatar address={token.address} size="xl" />
                 <VStack>
@@ -39,13 +42,28 @@ export function PoolTokenPill({ token }: Props) {
 
 export function PoolOpenGraph() {
     const { pool } = usePool();
+    const { chainId } = useNetworkConfig();
 
     return (
         <Box fontSize="xl">
+            <Box position="relative" top="297.5px" left="925px">
+                <Image
+                    src={chainId === '10' ? InvestMastheadOpImage : InvestMastheadImage}
+                    width="208.62px"
+                    height="68px"
+                    alt="masthead"
+                />
+            </Box>
             <Grid templateColumns={'1fr'} width="1200px" height="630px" id="og" p="50px">
                 <GridItem>
-                    <VStack width="full" alignItems="flex-start" mb="8">
-                        <Text fontWeight="bold" fontSize="5rem" mr="4">
+                    <VStack height="full" justify="space-between">
+                        <Text
+                            fontWeight="bold"
+                            fontSize="5rem"
+                            mr="4"
+                            borderBottomWidth="5px"
+                            borderBottomColor="beets.base.500"
+                        >
                             {pool.name}
                         </Text>
                         <Wrap>
