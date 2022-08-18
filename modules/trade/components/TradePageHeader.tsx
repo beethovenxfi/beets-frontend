0d4cc60b9c6cdd3962deaa8d34ade9ec4043e6d3
Daniel <danielBeets@protonmail.com>
2022-08-18 11:57:18 +0200

    add info button

--- modules/trade/components/TradePageHeader.tsx
+++ modules/trade/components/TradePageHeader.tsx
@@ -1,9 +1,9 @@
-import { Flex, HStack, Text } from '@chakra-ui/react';
+import { Box, Flex, HStack, Text } from '@chakra-ui/react';
 import { tokenFormatAmount } from '~/lib/services/token/token-util';
-import numeral from 'numeral';
 import { useTradeData } from '~/modules/trade/lib/useTradeData';
 import { useTradeChart } from '~/modules/trade/lib/useTradeChart';
 import { PercentChangeBadge } from '~/components/badge/PercentChangeBadge';
+import { InfoButton } from '~/components/info-button/InfoButton';
 
 export function TradePageHeader() {
     const { tokenOut, tokenIn, currentRatio } = useTradeData();
@@ -12,19 +12,24 @@ export function TradePageHeader() {
 
     return (
         <>
-            <Text fontSize="2xl" fontWeight="bold" color="white">
-                1
-                <Text as="span" fontSize="lg" fontWeight="normal" color="gray.100">
-                    {' '}
-                    {tokenOut?.symbol}
-                    {' = '}
+            <Flex>
+                <Text fontSize="2xl" fontWeight="bold" color="white">
+                    1
+                    <Text as="span" fontSize="lg" fontWeight="normal" color="gray.100">
+                        {' '}
+                        {tokenOut?.symbol}
+                        {' = '}
+                    </Text>
+                    {tokenFormatAmount(currentRatio)}
+                    <Text as="span" fontSize="lg" fontWeight="normal" color="gray.100">
+                        {' '}
+                        {tokenIn?.symbol}
+                    </Text>
                 </Text>
-                {tokenFormatAmount(currentRatio)}
-                <Text as="span" fontSize="lg" fontWeight="normal" color="gray.100">
-                    {' '}
-                    {tokenIn?.symbol}
-                </Text>
-            </Text>
+                <Box display="flex" alignItems="flex-end" mb="6px" ml="1">
+                    <InfoButton infoText="Global average price" />
+                </Box>
+            </Flex>
             {percentChange !== null && (
                 <HStack>
                     <PercentChangeBadge percentChange={percentChange} />
