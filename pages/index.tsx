2539c563ce56f3fd00e06bc469aa81852739f517
Daniel <danielBeets@protonmail.com>
2022-08-19 14:05:44 +0200

    add v1 redirects for pool detail, pools ,swap

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,35 @@ import { Home } from '~/modules/home/Home';
 import Head from 'next/head';
 import { initializeApolloClient, loadApolloState } from '~/apollo/client';
 import { GetHomeData } from '~/apollo/generated/operations';
+import { useEffect, useRef } from 'react';
+import { useRouter } from 'next/router';
+
+function useV1Redirect() {
+    const { push } = useRouter();
+    const redirectChecked = useRef(false);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window.location.hash && !redirectChecked.current) {
+            const hash = window.location.hash;
+
+            redirectChecked.current = true;
+
+            if (hash.indexOf('#/pool/') === 0) {
+                const poolId = hash.slice(7, 74);
+
+                push(`/pool/${poolId}`);
+            } else if (hash.indexOf('#/pools') === 0) {
+                push('/pools');
+            } else if (hash.indexOf('#/trade') === 0) {
+                push('/swap');
+            }
+        }
+    });
+}
 
 function HomePage() {
+    useV1Redirect();
+
     return (
         <>
             <Head>
