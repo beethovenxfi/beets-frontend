17b91d0f7dbdee5f16a6c406c352221210d87a05
Daniel <danielBeets@protonmail.com>
2022-12-20 12:09:37 +0800

    update text

--- components/boosted-badge/lib/boosted-by-tooltips.ts
+++ components/boosted-badge/lib/boosted-by-tooltips.ts
@@ -4,5 +4,5 @@ export const BoostedByTooltips: { [type: string]: string } = {
     'reaper-aave-granary':
         'This pool deposits a portion of its assets into Reaper.Farm vault(s) to generate boosted yield. The vault(s) are configured to deposit assets to Aave & Granary in supply-only mode.',
     overnight:
-        "This pool deposits a significant portion of its assets into Overnight's USD+ and DAI+ to generate boosted yield. Both USD+ and DAI+ utilize a multistrat architecture, assets are distributed to generate the highest risk-adjusted yield.",
+        "This pool deposits a significant portion of its assets into Overnight's USD+ and DAI+ to generate boosted yield. Both USD+ and DAI+ utilize a multistrat architecture, assets are distributed across many protocols to generate the highest risk-adjusted yield.",
 };
