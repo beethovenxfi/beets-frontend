e2bf70a29bfbbac5e1699c5314ef8c92c5885bb9
1saf <me@ansaf.net>
2022-12-21 23:29:24 +1100

    update: return spacing

--- modules/reliquary/components/RelicStats.tsx
+++ modules/reliquary/components/RelicStats.tsx
@@ -17,6 +17,8 @@ import { InfoButton } from '~/components/info-button/InfoButton';
 import TokenAvatar from '~/components/token/TokenAvatar';
 import { networkConfig } from '~/lib/config/network-config';
 import { useRelicDepositBalance } from '~/modules/reliquary/lib/useRelicDepositBalance';
+import RelicAchievements from './RelicAchievements';
+import RelicMaturity from './RelicMaturity';
 
 export function RelicStats() {
     const { data, relicBalanceUSD } = useRelicDepositBalance();
@@ -29,91 +31,73 @@ export function RelicStats() {
 
     return (
         <>
-            <Card p="4" width="full">
-                <VStack width="full" spacing="8" justifyContent="flex-start">
-                    <VStack width="full" spacing="0" alignItems="flex-start">
-                        <Heading lineHeight="1rem" fontWeight="semibold" size="" color="beets.base.50">
-                            Relic APR
-                        </Heading>
-                        <HStack>
-                            <div className="apr-stripes">{numeral(pool.dynamicData.apr.total).format('0.00%')}</div>
-                            <AprTooltip onlySparkles data={pool.dynamicData.apr} />
+            <VStack spacing="4">
+                <Card p="4" width="full">
+                    <VStack width="full" spacing="8" justifyContent="flex-start">
+                        <VStack width="full" spacing="0" alignItems="flex-start">
+                            <Heading lineHeight="1rem" fontWeight="semibold" size="" color="beets.base.50">
+                                Relic APR
+                            </Heading>
+                            <HStack>
+                                <div className="apr-stripes">{numeral(pool.dynamicData.apr.total).format('0.00%')}</div>
+                                <AprTooltip onlySparkles data={pool.dynamicData.apr} />
+                            </HStack>
+                            <Text color="orange">1.8x maturity boost</Text>
+                        </VStack>
+                        <HStack width="full" spacing="12" alignItems="flex-start">
+                            <VStack spacing="0" alignItems="flex-start">
+                                <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
+                                    Pending Rewards
+                                </Text>
+                                <Text color="white" fontSize="1.75rem">
+                                    {numberFormatUSDValue(999.99)}
+                                </Text>
+                            </VStack>
                         </HStack>
-                        <Text color="orange">1.8x maturity boost</Text>
                     </VStack>
-                    <HStack width="full" spacing="12" alignItems="flex-start">
-                        <VStack spacing="0" alignItems="flex-start">
-                            <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
-                                Pending Rewards
-                            </Text>
-                            <Text color="white" fontSize="1.75rem">
-                                {numberFormatUSDValue(999.99)}
-                            </Text>
-                        </VStack>
-                    </HStack>
-                </VStack>
-                <Button mt="4" width="full" variant="primary">
-                    Claim now
-                </Button>
-            </Card>
-            <Card p="4" width="full">
-                <VStack spacing="8">
-                    <HStack width="full" spacing="12" alignItems="flex-start">
-                        <VStack spacing="0" alignItems="flex-start">
-                            <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
-                                My Liquidity
-                            </Text>
-                            <Text color="white" fontSize="1.75rem">
-                                {numberFormatUSDValue(relicBalanceUSD)}
-                            </Text>
-                        </VStack>
-                    </HStack>
+                    <Button mt="4" width="full" variant="primary">
+                        Claim now
+                    </Button>
+                </Card>
+                <Card p="4" width="full">
+                    <VStack spacing="8">
+                        <HStack width="full" spacing="12" alignItems="flex-start">
+                            <VStack spacing="0" alignItems="flex-start">
+                                <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
+                                    My Liquidity
+                                </Text>
+                                <Text color="white" fontSize="1.75rem">
+                                    {numberFormatUSDValue(relicBalanceUSD)}
+                                </Text>
+                            </VStack>
+                        </HStack>
 
-                    <VStack width="full" spacing="3" alignItems="flex-start">
-                        <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
-                            Token breakdown
-                        </Text>
-                        <HStack>
-                            <TokenAmountPill
-                                key={`relic-token-${config.fbeets.address}`}
-                                address={config.fbeets.address}
-                                amount={relic.amount}
-                            />
-                            <Text fontWeight="bold">OR</Text>
-                            {data &&
-                                data.map((token) => (
-                                    <TokenAmountPill
-                                        key={`relic-token-${token.address}`}
-                                        address={token.address}
-                                        amount={token.amount}
-                                    />
-                                ))}
-                        </HStack>
+                        <VStack width="full" spacing="3" alignItems="flex-start">
+                            <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
+                                Token breakdown
+                            </Text>
+                            <HStack>
+                                <TokenAmountPill
+                                    key={`relic-token-${config.fbeets.address}`}
+                                    address={config.fbeets.address}
+                                    amount={relic.amount}
+                                />
+                                <Text fontWeight="bold">OR</Text>
+                                {data &&
+                                    data.map((token) => (
+                                        <TokenAmountPill
+                                            key={`relic-token-${token.address}`}
+                                            address={token.address}
+                                            amount={token.amount}
+                                        />
+                                    ))}
+                            </HStack>
+                        </VStack>
                     </VStack>
-                </VStack>
-            </Card>
-            <Card p="4" width="full">
-                <VStack spacing="8">
-                    <HStack width="full" spacing="12" alignItems="flex-start">
-                        <VStack spacing="0" alignItems="flex-start">
-                            <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
-                                Achievements
-                            </Text>
-                        </VStack>
-                    </HStack>
-                </VStack>
-            </Card>
-            <Card p="4" width="full">
-                <VStack spacing="8">
-                    <HStack width="full" spacing="12" alignItems="flex-start">
-                        <VStack spacing="0" alignItems="flex-start">
-                            <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
-                                Maturity
-                            </Text>
-                        </VStack>
-                    </HStack>
-                </VStack>
-            </Card>
+                </Card>
+                <RelicAchievements />
+                <RelicMaturity />
+            </VStack>
             <Flex width="full" alignItems="flex-start" flex={1} flexDirection="column">
                 <VStack spacing="0" alignItems="flex-start" mb="4" px="2">
                     <Text lineHeight="1rem" fontWeight="semibold" fontSize="sm" color="beets.base.50">
