a828dc4fb29f7db773902f0ca76d4801b6891d6c
Ansaf Ahmed <me@ansaf.net>
2023-01-10 01:40:43 +1100

    feature: hook for legacy fbeets balances

---
+++ modules/reliquary/lib/useLegacyFbeetsBalance.ts
@@ -0,0 +1,44 @@
+import { AmountHumanReadable } from '~/lib/services/token/token-types';
+import { useProvider } from 'wagmi';
+import { useQuery } from 'react-query';
+import { freshBeetsService } from '~/lib/services/staking/fresh-beets.service';
+import { useUserAccount } from '~/lib/user/useUserAccount';
+import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
+import { useUserBalances } from '~/lib/user/useUserBalances';
+import { keyBy } from 'lodash';
+
+export function useLegacyFBeetsBalance() {
+    const { userAddress } = useUserAccount();
+    const provider = useProvider();
+    const networkConfig = useNetworkConfig();
+    const { userBalances: legacyBalances = [], isLoading: isLoadingLegacyBalances } = useUserBalances([
+        networkConfig.fbeets.address,
+    ]);
+
+    const userBalancesMap = keyBy(legacyBalances, 'address');
+    const legacyFbeetsBalance = userBalancesMap[networkConfig.fbeets.address]?.amount || '0';
+
+    const { data: stakedBalance = '0', isLoading: isLoadingStakedBalance} = useQuery(
+        ['legacyFbeetsBalances', userAddress || ''],
+        async (): Promise<AmountHumanReadable> => {
+            if (!userAddress) {
+                return '0';
+            }
+
+            return freshBeetsService.getUserStakedBalance({
+                userAddress,
+                farmId: networkConfig.fbeets.farmId,
+                provider,
+                fBeetsRatio: '1',
+            });
+        },
+        {},
+    );
+
+    return {
+        staked: stakedBalance,
+        unstaked: legacyFbeetsBalance,
+        isLoading: isLoadingStakedBalance || isLoadingLegacyBalances,
+        total: parseFloat(stakedBalance || '0') + parseFloat(legacyFbeetsBalance || '0'),
+    };
+}
