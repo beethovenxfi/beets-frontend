f3b811a0fe352cf1aa4c58d4de40a3fe18a3493d
groninge <groninge@gmail.com>
2023-01-10 10:51:35 +0100

    add images

--- modules/reliquary/ReliquaryLanding.tsx
+++ modules/reliquary/ReliquaryLanding.tsx
@@ -1,8 +1,12 @@
-import { Button, HStack, ListItem, Text, UnorderedList, VStack, Image, Stack } from '@chakra-ui/react';
+import { Button, HStack, ListItem, Text, UnorderedList, VStack, Stack } from '@chakra-ui/react';
 import React from 'react';
 import { InfoButton } from '~/components/info-button/InfoButton';
 import { RelicCarousel } from './components/RelicCarousel';
 import ReliquaryMyStats from './components/ReliquaryMyStats';
+import Rq1Image from '~/assets/images/rq-1.png';
+import Rq2Image from '~/assets/images/rq-2.png';
+import Rq3Image from '~/assets/images/rq-3.png';
+import Image from 'next/image';
 
 const buttonWidth = {
     base: 'full',
@@ -16,11 +20,29 @@ const infoButtonLabelProps = {
     color: 'beets.base.50',
 };
 
+const rqImages = [
+    {
+        src: Rq1Image,
+        alt: 'fBEETS',
+        info: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet',
+    },
+    {
+        src: Rq2Image,
+        alt: 'Reliquary',
+        info: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet',
+    },
+    {
+        src: Rq3Image,
+        alt: 'fBEETS',
+        info: 'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet',
+    },
+];
+
 export default function ReliquaryLanding() {
     return (
         <Stack direction="column">
             <Stack direction={['column', 'row']} alignItems="stretch" mb="16">
-                <VStack alignItems="flex-start" justifyContent="space-between" mr="16">
+                <VStack alignItems="flex-start" justifyContent="space-between" mr="16" mb={{ base: 4, lg: undefined }}>
                     <Text color="beets.green" fontSize="sm">
                         rfBEETS
                     </Text>
@@ -32,7 +54,6 @@ export default function ReliquaryLanding() {
                     >
                         Extraordinary earnings & voting power
                     </Text>
-
                     <UnorderedList pl="5">
                         <ListItem>Consectetur adipiscing elit</ListItem>
                         <ListItem>Integer molestie lorem at massa</ListItem>
@@ -47,31 +68,14 @@ export default function ReliquaryLanding() {
                         </Button>
                     </HStack>
                 </VStack>
-                <Stack direction={['column', 'row']}>
-                    <VStack>
-                        <Image borderRadius="xl" src="https://via.placeholder.com/400" alt="placeholder" />
-                        <InfoButton
-                            labelProps={infoButtonLabelProps}
-                            label="fBEETS"
-                            infoText="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet"
-                        />
-                    </VStack>
-                    <VStack>
-                        <Image borderRadius="xl" src="https://via.placeholder.com/400" alt="placeholder" />
-                        <InfoButton
-                            labelProps={infoButtonLabelProps}
-                            label="Reliquary"
-                            infoText="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet"
-                        />
-                    </VStack>
-                    <VStack>
-                        <Image borderRadius="xl" src="https://via.placeholder.com/400" alt="placeholder" />
-                        <InfoButton
-                            labelProps={infoButtonLabelProps}
-                            label="rfBEETS"
-                            infoText="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet"
-                        />
-                    </VStack>
+                <Stack direction={['column', 'row']} spacing="8">
+                    {rqImages.map((image, index) => (
+                        // TODO: why is the 'mb' style overwritten for the seconf & third image?
+                        <VStack key={index} mb={{ base: 4, lg: undefined }}>
+                            <Image src={image.src} alt={image.alt} placeholder="blur" style={{ borderRadius: '8px' }} />
+                            <InfoButton labelProps={infoButtonLabelProps} label={image.alt} infoText={image.info} />
+                        </VStack>
+                    ))}
                 </Stack>
             </Stack>
             <RelicCarousel />
