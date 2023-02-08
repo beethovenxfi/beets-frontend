396ac70970f32a8ff780ddec73657e01bc3e0cfe
groninge <groninge@gmail.com>
2023-02-08 14:10:05 +0100

    update page

--- modules/reliquary/detail/ReliquaryPool.tsx
+++ modules/reliquary/detail/ReliquaryPool.tsx
@@ -1,58 +1,22 @@
-import { Box, Grid, GridItem, HStack, VStack, Text, Button } from '@chakra-ui/react';
+import { Box, Grid, GridItem, HStack, VStack, Button } from '@chakra-ui/react';
 import PoolHeader from '~/modules/pool/detail/components/PoolHeader';
 import { PoolComposition } from '~/modules/pool/detail/components/composition/PoolComposition';
-import PoolStats from './components/stats/PoolStats';
-import { PoolTransactions } from './components/transactions/PoolTransactions';
+import PoolStats from '~/modules/pool/detail/components/stats/PoolStats';
+import { PoolTransactions } from '~/modules/pool/detail/components/transactions/PoolTransactions';
 import { PoolDetailCharts } from '~/modules/pool/detail/components/PoolDetailCharts';
-import { PoolProvider, usePool } from '~/modules/pool/lib/usePool';
-import { usePoolUserBptBalance } from '~/modules/pool/lib/usePoolUserBptBalance';
-import { PoolFbeetsWarning } from '~/modules/pool/detail/components/PoolFbeetsWarning';
-import { useEffect } from 'react';
-import { ToastType, useToast } from '~/components/toast/BeetsToast';
-import { useLegacyFBeetsBalance } from '~/modules/reliquary/lib/useLegacyFbeetsBalance';
-import ReliquaryMigrateModal from '~/modules/reliquary/components/ReliquaryMigrateModal';
-import { TokensProvider } from '~/lib/global/useToken';
+import { NextLink } from '~/components/link/NextLink';
 
 export function ReliquaryPool() {
-    const { pool, isFbeetsPool } = usePool();
-    const { hasBpt } = usePoolUserBptBalance();
-    const { total } = useLegacyFBeetsBalance();
-    const { showToast, removeToast } = useToast();
-
-    useEffect(() => {
-        return () => {
-            removeToast('migrate-fbeets');
-        };
-    }, []);
-
-    useEffect(() => {
-        if (total > 0 && isFbeetsPool) {
-            showToast({
-                id: 'migrate-fbeets',
-                type: ToastType.Warn,
-                content: (
-                    <HStack>
-                        <Text>You can migrate your legacy fBEETS position to a relic</Text>
-                        <TokensProvider>
-                            <PoolProvider pool={pool}>
-                                <ReliquaryMigrateModal />
-                            </PoolProvider>
-                        </TokensProvider>
-                    </HStack>
-                ),
-            });
-        } else {
-            removeToast('migrate-fbeets');
-        }
-    }, [total]);
-
     return (
         <Box marginBottom="8">
             <PoolHeader />
             <VStack width="full" spacing="4">
-                {isFbeetsPool && hasBpt && total === 0 && <PoolFbeetsWarning />}
                 <HStack width="full" justifyContent="flex-end">
-                    <Button>Go to maBEETS</Button>
+                    <NextLink href="/mabeets" chakraProps={{ _hover: { textDecoration: 'none' } }}>
+                        <Button variant="primary" width={{ base: '130px', lg: '160px' }}>
+                            Go to maBEETS
+                        </Button>
+                    </NextLink>
                 </HStack>
                 <Grid gap="4" templateColumns={{ base: '1fr', lg: '300px 1fr' }} width="full">
                     <GridItem>
