cbc5683ba0058c9314d55470ec65d51a8a9540d9
groninge <groninge@gmail.com>
2023-02-10 14:20:12 +0100

    add image overlay

--- modules/reliquary/invest/components/ReliquaryInvestImage.tsx
+++ modules/reliquary/invest/components/ReliquaryInvestImage.tsx
@@ -1,10 +1,31 @@
 import { Box } from '@chakra-ui/react';
-import React, { useContext } from 'react';
+import React from 'react';
+import Image from 'next/image';
 import { useCurrentStep } from '../../lib/useReliquaryCurrentStep';
+import styles from '~/styles/Reliquary.module.css';
+import Background from '~/assets/images/reliquary/background.png';
+import ReliquaryInvest from '~/assets/images/reliquary/complete.png';
 
 export function ReliquaryInvestImage() {
     const { currentStep } = useCurrentStep();
-    console.log({ currentStep });
 
-    return <Box w="full">{currentStep}</Box>;
+    function getCurrentStepImage(currentStep: string) {
+        switch (currentStep) {
+            // more cases
+            case 'reliquary-invest':
+            default:
+                return ReliquaryInvest;
+        }
+    }
+
+    return (
+        <Box mt="4">
+            <div className={styles.container}>
+                <div className={styles.overlapGrid}>
+                    <Image src={Background} alt="background" />
+                    <Image src={getCurrentStepImage(currentStep)} alt="reliquary" />
+                </div>
+            </div>
+        </Box>
+    );
 }
