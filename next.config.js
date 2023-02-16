6bc766e668bda786717ba520907f4323c1e16306
groninge <groninge@gmail.com>
2023-02-16 16:44:15 +0100

    add config for i18next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const transpiled = require('next-transpile-modules')(['echarts', 'zrender']);
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
     reactStrictMode: true,
@@ -10,6 +11,8 @@ const nextConfig = {
         config.resolve.alias['~'] = path.join(__dirname, 'src');
         return config;
     },
+
+    i18n,
 };
 
 module.exports = transpiled(nextConfig);
