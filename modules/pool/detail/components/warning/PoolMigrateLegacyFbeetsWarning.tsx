c410055a74a7d9475f9e1e97f9f97030112b68cd
groninge <groninge@gmail.com>
2023-02-20 14:06:02 +0100

    fix toast

--- modules/pool/detail/components/warning/PoolMigrateLegacyFbeetsWarning.tsx
+++ modules/pool/detail/components/warning/PoolMigrateLegacyFbeetsWarning.tsx
@@ -1,40 +1,51 @@
-import { HStack, Text } from '@chakra-ui/react';
-import { PoolProvider, usePool } from '~/modules/pool/lib/usePool';
+import { Box, Button, Link, Stack, Text } from '@chakra-ui/react';
+import { usePool } from '~/modules/pool/lib/usePool';
 import { ToastType, useToast } from '~/components/toast/BeetsToast';
 import { useEffect } from 'react';
 import { useLegacyFBeetsBalance } from '~/modules/reliquary/lib/useLegacyFbeetsBalance';
-import { TokensProvider } from '~/lib/global/useToken';
-import ReliquaryMigrateModal from '~/modules/reliquary/components/ReliquaryMigrateModal';
 
 export function PoolMigrateLegacyFbeetsWarning() {
-    const { pool, isFbeetsPool } = usePool();
+    const { isFbeetsPool } = usePool();
     const { total } = useLegacyFBeetsBalance();
     const { showToast, removeToast } = useToast();
 
     useEffect(() => {
         return () => {
-            removeToast('migrate-fbeets');
+            removeToast('migrate-to-mabeets');
         };
     }, []);
 
     useEffect(() => {
         if (total > 0 && isFbeetsPool) {
             showToast({
-                id: 'migrate-fbeets',
+                id: 'migrate-to-mabeets',
                 type: ToastType.Warn,
                 content: (
-                    <HStack>
-                        <Text>You can migrate your legacy fBEETS position to a relic</Text>
-                        <TokensProvider>
-                            <PoolProvider pool={pool}>
-                                <ReliquaryMigrateModal />
-                            </PoolProvider>
-                        </TokensProvider>
-                    </HStack>
+                    <Stack
+                        direction={['column', 'row']}
+                        spacing="4"
+                        alignItems="center"
+                        justifyContent={{ base: 'stretch', xl: undefined }}
+                    >
+                        <Box>
+                            <Text>You can migrate your v1 fBEETS position on the maBEETS page.</Text>
+                        </Box>
+                        <Button
+                            w={{ base: 'full', xl: 'inherit' }}
+                            colorScheme="orange"
+                            variant="solid"
+                            backgroundColor="orange.300"
+                            as={Link}
+                            href="/mabeets"
+                            _hover={{ textDecoration: 'none', backgroundColor: 'orange.400' }}
+                        >
+                            Go to maBEETS
+                        </Button>
+                    </Stack>
                 ),
             });
         } else {
-            removeToast('migrate-fbeets');
+            removeToast('migrate-to-mabeets');
         }
     }, [total]);
 
