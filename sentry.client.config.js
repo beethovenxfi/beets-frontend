7da3ca0366b698e6e15a66cc0e3bf65318c86796
groninge <groninge@gmail.com>
2023-02-21 16:49:44 +0100

    only enable sentry on production

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,6 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
+    enabled: process.env.NODE_ENV === 'production',
     dsn: SENTRY_DSN,
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
