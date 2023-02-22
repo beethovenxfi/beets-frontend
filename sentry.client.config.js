a3b17c537033237e3a09e028e47fda1f6a8b424f
groninge <groninge@gmail.com>
2023-02-22 08:58:42 -0500

    Revert "configure integrations"
    This reverts commit cc13ec0a57ba01c93c4e6172f5f31979a04977b0.

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,17 +3,11 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
-import { CaptureConsole as CaptureConsoleIntegration } from '@sentry/integrations';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
     dsn: SENTRY_DSN,
-    integrations: [
-        new CaptureConsoleIntegration({
-            levels: ['error'],
-        }),
-    ],
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
     // ...
