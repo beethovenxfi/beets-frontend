a51256a672924093aa88a10b4104ab437fb8494f
groninge <groninge@gmail.com>
2023-02-23 11:48:35 +0100

    add empty image for animation

--- modules/reliquary/invest/components/ReliquaryInvestImage.tsx
+++ modules/reliquary/invest/components/ReliquaryInvestImage.tsx
@@ -11,12 +11,13 @@ import ApproveRelic from '~/assets/images/reliquary/approve-relic.png';
 import MigrateLegacy from '~/assets/images/reliquary/migrate-legacy.png';
 import ApproveBeets from '~/assets/images/reliquary/approve-beets.png';
 import ApproveWftm from '~/assets/images/reliquary/approve-wftm.png';
+import Empty from '~/assets/images/reliquary/1x1-transparent.png';
 import { AnimatePresence, motion } from 'framer-motion';
 
 export function ReliquaryInvestImage() {
     const { currentStep } = useCurrentStep();
 
-    function getCurrentStepImage(currentStep: string) {
+    function getCurrentStepImage(currentStep: string | null) {
         switch (currentStep) {
             case 'wFTM':
                 return ApproveWftm;
@@ -32,8 +33,9 @@ export function ReliquaryInvestImage() {
                 return ApproveVault;
             case 'reliquary-migrate':
             case 'reliquary-invest':
-            default:
                 return ReliquaryInvest;
+            default:
+                return Empty;
         }
     }
 
@@ -54,7 +56,7 @@ export function ReliquaryInvestImage() {
                                     opacity: { duration: 0.25 },
                                 }}
                             >
-                                {currentStep && <Image src={getCurrentStepImage(currentStep)} alt="reliquary" />}
+                                <Image src={getCurrentStepImage(currentStep)} alt="reliquary" />
                             </motion.div>
                         </AnimatePresence>
                     </div>
