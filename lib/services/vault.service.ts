6c10ccade648c04d98c25d1be6b2ee6276cb6903
Daniel <danielBeets@protonmail.com>
2023-03-01 08:40:17 +0100

    better formatted response

--- lib/services/vault.service.ts
+++ lib/services/vault.service.ts
@@ -2,13 +2,14 @@ import { BaseProvider } from '@ethersproject/providers';
 import VaultAbi from '~/lib/abi/VaultAbi.json';
 import { BigNumber, Contract } from 'ethers';
 import { networkConfig } from '~/lib/config/network-config';
+import { AmountScaled } from '~/lib/services/token/token-types';
 
 interface GetPoolTokensInput {
     poolId: string;
     provider: BaseProvider;
 }
 
-interface PoolTokens {
+interface PoolTokensResponse {
     balances: BigNumber[];
     tokens: string[];
 }
@@ -16,13 +17,17 @@ interface PoolTokens {
 export class VaultService {
     constructor(private readonly vaultAddress: string) {}
 
-    public async getPoolTokens({ poolId, provider }: GetPoolTokensInput): Promise<any> {
+    public async getPoolTokens({
+        poolId,
+        provider,
+    }: GetPoolTokensInput): Promise<{ address: string; balanceScaled: AmountScaled }[]> {
         const vaultContract = new Contract(this.vaultAddress, VaultAbi, provider);
-        const poolTokens: PoolTokens = await vaultContract.getPoolTokens(poolId);
-        const poolTokensArray = [poolTokens.tokens, poolTokens.balances];
+        const poolTokens: PoolTokensResponse = await vaultContract.getPoolTokens(poolId);
+
+        return poolTokens.tokens.map((token, index) => ({ address: token, balanceScaled: poolTokens.balances[index] }));
 
         // transpose 2d array: [['A', 'B', 'C'], [1, 2, 3]] => [['A', 1], ['B', 2], ['C', 3]]
-        return poolTokensArray[0].map((_, colIndex) => poolTokensArray.map((row) => row[colIndex]));
+        //return poolTokensArray[0].map((_, colIndex) => poolTokensArray.map((row) => row[colIndex]));
     }
 }
 
