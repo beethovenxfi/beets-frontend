9c60f7cc4823f502fc2a9e47be56bb88a8e981f3
groninge <groninge@gmail.com>
2023-03-01 10:16:58 +0100

    remove commented out code

--- lib/services/vault.service.ts
+++ lib/services/vault.service.ts
@@ -25,9 +25,6 @@ export class VaultService {
         const poolTokens: PoolTokensResponse = await vaultContract.getPoolTokens(poolId);
 
         return poolTokens.tokens.map((token, index) => ({ address: token, balanceScaled: poolTokens.balances[index] }));
-
-        // transpose 2d array: [['A', 'B', 'C'], [1, 2, 3]] => [['A', 1], ['B', 2], ['C', 3]]
-        //return poolTokensArray[0].map((_, colIndex) => poolTokensArray.map((row) => row[colIndex]));
     }
 }
 
