bc4abc0897b72b845f35fc2f2bc9b01d112e620d
groninge <groninge@gmail.com>
2023-03-07 17:18:12 +0100

    use sorQuery hook

--- modules/pool/lib/usePool.tsx
+++ modules/pool/lib/usePool.tsx
@@ -13,7 +13,7 @@ import { TokenBase } from '~/lib/services/token/token-types';
 import { uniqBy } from 'lodash';
 import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
 import { isSameAddress } from '@balancer-labs/sdk';
-import { usePoolTokens } from '~/lib/global/usePoolTokens';
+import { usePoolGetPoolData } from './usePoolGetPoolData';
 
 export interface PoolContextType {
     pool: GqlPoolUnion;
@@ -44,8 +44,9 @@ export function PoolProvider({ pool: poolFromProps, children }: { pool: GqlPoolU
     const pool = (data?.pool || poolFromProps) as GqlPoolUnion;
     const poolService = poolGetServiceForPool(pool);
 
-    const { poolTokens } = usePoolTokens(pool.id);
     //TODO: inject the balances into the pool at the source, then the amounts will be correct everywhere by default.
+    const { data: poolData } = usePoolGetPoolData([pool.id]);
+    console.log({ poolData });
 
     const bpt: TokenBase = {
         address: pool.address,
