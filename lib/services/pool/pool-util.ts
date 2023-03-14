738c9b0d896ad6ecade780e26000a03f5114b83f
groninge <groninge@gmail.com>
2023-03-14 08:25:23 +0100

    add final calculation & comments

--- lib/services/pool/pool-util.ts
+++ lib/services/pool/pool-util.ts
@@ -237,42 +237,57 @@ export function updateBalances(
         return;
     }
 
-    let updates: any[] = [];
     const { balances, totalSupplies } = poolData;
 
+    // keep track of where we are and push the complete path to updates
     let depth = 1;
     let lastDepth = 1;
     let updatePath: string[] = [];
+    let updates: any[] = []; // TODO: type this
+
+    // keep track of some values for nested pool balances
+    let parentTokenBalances: string[] = [];
     let totalSharesValue: string;
+    let totalSharesValues: string[] = [];
 
     let traverse = (pool: GqlPoolUnion | GqlPoolPhantomStableNested | GqlPoolLinearNested) => {
         const poolIdIdx = poolIds.findIndex((id) => id === pool.id);
+        const hasTotalShares = 'totalShares' in pool;
 
-        if ('totalShares' in pool) {
+        if (hasTotalShares) {
             const totalSharesPath = [...updatePath, 'pool', 'totalShares'].join('.');
             totalSharesValue = formatFixed(totalSupplies[poolIdIdx], 18);
             updates.push({ path: totalSharesPath, value: totalSharesValue });
+            // remember the value for later use
+            totalSharesValues.push(totalSharesValue);
         }
 
-        // TODO: calculate new balance from updated totalShares
-
         pool.tokens.forEach((token, tokenIdx) => {
+            // at the top level just push 'tokens.<tokenIdx>' or drop everything when coming back to it
             if (depth === 1) {
                 if (updatePath.length) {
                     updatePath = [];
                 }
                 updatePath.push('tokens', tokenIdx.toString());
+
+                // drop the last '<parentTokenIdx>.pool.tokens.<tokenIdx>' and add the next '<tokenIdx>' when going up a level
             } else if (lastDepth > depth) {
                 updatePath.splice(-4);
                 updatePath.push(tokenIdx.toString());
+
+                // keep adding 'pool.tokens.<tokenIdx>' as we go all the way down
             } else if (updatePath[updatePath.length - 1] === tokenIdx.toString()) {
                 updatePath.push('pool', 'tokens', tokenIdx.toString());
+
+                // back down a level through the next branch
             } else if (
                 depth > lastDepth &&
                 updatePath[updatePath.length - 1] !== tokenIdx.toString() &&
                 updatePath.length < depth * 3 - 1
             ) {
                 updatePath.push('pool', 'tokens', tokenIdx.toString());
+
+                // at the leaves we just swap '<tokenIdx>' for the next one
             } else {
                 updatePath.splice(-1);
                 updatePath.push(tokenIdx.toString());
@@ -284,9 +299,16 @@ export function updateBalances(
                 if (token.index === balanceIdx) {
                     const balanceValue = formatFixed(balance, token.decimals);
 
+                    // use the last stored values to calculate the percentage of nested supply
+                    const nestedBalanceValue = (
+                        (parseFloat(parentTokenBalances[parentTokenBalances.length - 1]) /
+                            parseFloat(totalSharesValues[totalSharesValues.length - 1])) *
+                        parseFloat(balanceValue)
+                    ).toString();
+
                     return [
                         { path: totalBalancePath, value: formatFixed(balance, token.decimals) },
-                        { path: balancePath, value: balanceValue },
+                        { path: balancePath, value: hasTotalShares ? nestedBalanceValue : balanceValue },
                     ];
                 } else {
                     return [];
@@ -296,16 +318,26 @@ export function updateBalances(
             updates.push(balanceObj);
             if ('pool' in token) {
                 lastDepth = depth++;
+                // remember the value for later use
+                parentTokenBalances.push(token.balance);
+
                 traverse(token.pool);
+
                 lastDepth = depth--;
+                // done with the level so dump the last values
+                parentTokenBalances.pop();
+                totalSharesValues.pop();
             }
         });
     };
 
     traverse(pool);
+
+    // completely flatten the updates array
     updates = updates.flat(2);
 
     let updatedPool = pool;
+    // cycle through the updates array and use updeep to update the values
     updates.forEach((update) => (updatedPool = u.updateIn(update.path, update.value, updatedPool)));
 
     return updatedPool as GqlPoolUnion;
