1e16e5b5934daa1f6557f2b07ab862e99db1160f
Daniel <danielBeets@protonmail.com>
2023-03-14 12:21:09 +0100

    hookup pass

--- lib/services/pool/pool-on-chain-balance.service.ts
+++ lib/services/pool/pool-on-chain-balance.service.ts
@@ -1,6 +1,14 @@
 import { BaseProvider } from '@ethersproject/providers';
-import { GqlPoolUnion } from '~/apollo/generated/graphql-codegen-generated';
+import {
+    GqlPoolLinearNested,
+    GqlPoolPhantomStableNested,
+    GqlPoolUnion,
+} from '~/apollo/generated/graphql-codegen-generated';
 import { SorQueriesTotalSupplyType, sorQueryService, SorQueryService } from '~/lib/services/pool/sor-query.service';
+import { AdditionalPoolData } from '~/lib/services/pool/pool-types';
+import { formatFixed } from '@ethersproject/bignumber';
+import { BigNumber } from 'ethers';
+import { cloneDeep, keyBy } from 'lodash';
 
 export class PoolOnChainBalanceService {
     constructor(private readonly sorQueryService: SorQueryService) {}
@@ -12,10 +20,37 @@ export class PoolOnChainBalanceService {
         pool: GqlPoolUnion;
         provider: BaseProvider;
     }): Promise<GqlPoolUnion> {
+        const balanceData = await this.getBalanceDataForPool({ pool, provider });
+        const dataMap = keyBy(balanceData, 'poolId');
+        const clone = cloneDeep(pool);
+
+        clone.dynamicData.totalShares = formatFixed(dataMap[pool.id].totalSupply, 18);
+
+        //TODO: finish building this out
+        for (const token of clone.tokens) {
+            if (token.__typename === 'GqlPoolTokenLinear' || token.__typename === 'GqlPoolTokenPhantomStable') {
+                token.pool.totalShares = formatFixed(dataMap[token.pool.id].totalSupply, 18);
+            }
+
+            if (token.__typename === 'GqlPoolTokenPhantomStable') {
+                for (const nestedToken of token.pool.tokens) {
+                    if (nestedToken.__typename === 'GqlPoolTokenLinear') {
+                        nestedToken.pool.totalShares = formatFixed(dataMap[nestedToken.pool.id].totalSupply, 18);
+                    }
+                }
+            }
+        }
+
         throw new Error('TODO');
     }
 
-    private async getBalanceDataForPool({ pool, provider }: { pool: GqlPoolUnion; provider: BaseProvider }) {
+    private async getBalanceDataForPool({
+        pool,
+        provider,
+    }: {
+        pool: GqlPoolUnion;
+        provider: BaseProvider;
+    }): Promise<{ poolId: string; balances: BigNumber[]; totalSupply: BigNumber }[]> {
         const poolIds: string[] = [pool.id];
         const totalSupplyTypes: SorQueriesTotalSupplyType[] = [this.sorQueryService.getTotalSupplyType(pool)];
 
@@ -28,22 +63,29 @@ export class PoolOnChainBalanceService {
             if (token.__typename === 'GqlPoolTokenPhantomStable') {
                 for (const nestedToken of token.pool.tokens) {
                     if (nestedToken.__typename === 'GqlPoolTokenLinear') {
-                        poolIds.push(token.pool.id);
-                        totalSupplyTypes.push(this.sorQueryService.getTotalSupplyType(token.pool));
+                        poolIds.push(nestedToken.pool.id);
+                        totalSupplyTypes.push(this.sorQueryService.getTotalSupplyType(nestedToken.pool));
                     }
                 }
             }
         }
 
-        return this.sorQueryService.getPoolData({
+        const { balances, totalSupplies } = await this.sorQueryService.getPoolData({
             poolIds,
             provider,
             config: {
                 loadTokenBalanceUpdatesAfterBlock: true,
+                blockNumber: 0,
                 loadTotalSupply: true,
                 totalSupplyTypes,
             },
         });
+
+        return poolIds.map((poolId, index) => ({
+            poolId,
+            balances: balances[index],
+            totalSupply: totalSupplies[index],
+        }));
     }
 }
 
