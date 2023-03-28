52668fa2d1c564040d2a00a9e1334e57ec535095
groninge <groninge@gmail.com>
2023-03-28 10:48:27 +0200

    resolve comments

--- modules/pool/detail/components/stats/PoolOverallStats.tsx
+++ modules/pool/detail/components/stats/PoolOverallStats.tsx
@@ -33,18 +33,16 @@ export default function PoolOverallStats() {
     const sharePricePercentChange = (sharePrice - sharePrice24hAgo) / sharePrice24hAgo;
     const beetsPerDay = parseFloat(pool.staking?.farm?.beetsPerBlock || '0') * (blocksData?.blocksPerDay || 0);
 
+    const rewards = pool.staking?.farm?.rewarders || pool.staking?.gauge?.rewards;
+    const rewardsMapped = rewards?.map(({ tokenAddress, rewardPerSecond }) => ({ tokenAddress, rewardPerSecond }));
+    const hasNonZeroRewards = rewardsMapped?.filter((reward) => reward.rewardPerSecond !== '0').length !== 0;
+
     const incentivesDailyValue =
         beetsPerDay * priceFor(networkConfig.beets.address) +
         sumBy(
-            pool.staking?.farm?.rewarders || [],
+            rewardsMapped || [],
             (rewarder) => priceFor(rewarder.tokenAddress) * parseFloat(rewarder.rewardPerSecond) * 86400,
-        ) +
-        sumBy(
-            pool.staking?.gauge?.rewards || [],
-            (reward) => priceFor(reward.tokenAddress) * parseFloat(reward.rewardPerSecond) * 86400,
         );
-
-    const rewards = pool.staking?.farm?.rewarders || pool.staking?.gauge?.rewards;
 
     return (
         <VStack spacing="4" width="full" alignItems="flex-start" px="2">
@@ -94,8 +92,8 @@ export default function PoolOverallStats() {
                     {numeral(data.fees24h).format('$0,0.00a')}
                 </Text>
             </VStack>
-            {incentivesDailyValue > 0 && (
-                <VStack spacing="0" alignItems="flex-start">
+            {(hasNonZeroRewards || beetsPerDay > 0) && (
+                <VStack spacing="1" alignItems="flex-start">
                     <InfoButton
                         labelProps={{
                             lineHeight: '1rem',
@@ -106,41 +104,41 @@ export default function PoolOverallStats() {
                         label="Liquidity incentives"
                         infoText={`Liquidity incentives are additional incentives available for this pool when you stake your BPT in the ${networkConfig.farmTypeName}. The daily value is an approximation based on current token prices and emissions.`}
                     />
-                    <Text color="white" fontSize="1.75rem">
-                        ~{numeral(incentivesDailyValue).format('$0,0.00a')}
-                        <Text as="span" fontSize="md">
-                            {' '}
-                            / day
+                    {incentivesDailyValue > 0 && (
+                        <Text color="white" fontSize="1.75rem">
+                            ~{numeral(incentivesDailyValue).format('$0,0.00a')}
+                            <Text as="span" fontSize="md">
+                                {' '}
+                                / day
+                            </Text>
                         </Text>
-                    </Text>
-                    <Box>
-                        {beetsPerDay > 0 && (
-                            <HStack spacing="1" mb="0.5">
-                                <TokenAvatar height="20px" width="20px" address={networkConfig.beets.address} />
-                                <Tooltip
-                                    label={`BEETS emissions are calculated per block, so daily emissions are an estimate based on an average block time over last 5,000 blocks. Avg block time: ${blocksData?.avgBlockTime}s.`}
-                                >
+                    )}
+                    {beetsPerDay > 0 && (
+                        <HStack spacing="1" mb="0.5">
+                            <TokenAvatar height="20px" width="20px" address={networkConfig.beets.address} />
+                            <Tooltip
+                                label={`BEETS emissions are calculated per block, so daily emissions are an estimate based on an average block time over last 5,000 blocks. Avg block time: ${blocksData?.avgBlockTime}s.`}
+                            >
+                                <Text fontSize="1rem" lineHeight="1rem">
+                                    {numeral(beetsPerDay).format('0,0')} / day
+                                </Text>
+                            </Tooltip>
+                        </HStack>
+                    )}
+                    {rewards &&
+                        rewards.map((reward) => {
+                            if (!reward || reward.rewardPerSecond === '0') {
+                                return null;
+                            }
+                            return (
+                                <HStack spacing="1" mb="0.5" key={reward.id}>
+                                    <TokenAvatar height="20px" width="20px" address={reward.tokenAddress} />
                                     <Text fontSize="1rem" lineHeight="1rem">
-                                        {numeral(beetsPerDay).format('0,0')} / day
+                                        {numeral(parseFloat(reward.rewardPerSecond) * 86400).format('0,0')} / day
                                     </Text>
-                                </Tooltip>
-                            </HStack>
-                        )}
-                        {rewards &&
-                            rewards.map((reward) => {
-                                if (!reward || reward.rewardPerSecond === '0') {
-                                    return null;
-                                }
-                                return (
-                                    <HStack spacing="1" mb="0.5" key={reward.id}>
-                                        <TokenAvatar height="20px" width="20px" address={reward.tokenAddress} />
-                                        <Text fontSize="1rem" lineHeight="1rem">
-                                            {numeral(parseFloat(reward.rewardPerSecond) * 86400).format('0,0')} / day
-                                        </Text>
-                                    </HStack>
-                                );
-                            })}
-                    </Box>
+                                </HStack>
+                            );
+                        })}
                 </VStack>
             )}
         </VStack>
