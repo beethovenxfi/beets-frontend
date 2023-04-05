fb59b99c24ae48d44dfc88ff08809397f9c17f26
groninge <groninge@gmail.com>
2023-04-05 15:01:59 +0200

    near final calc for proportional invest

--- modules/pool/invest/lib/usePoolJoinGetProportionalInvestmentAmount.ts
+++ modules/pool/invest/lib/usePoolJoinGetProportionalInvestmentAmount.ts
@@ -1,11 +1,14 @@
 import { useQuery } from 'react-query';
 import { useInvest } from '~/modules/pool/invest/lib/useInvest';
 import { sortBy, sum, sumBy } from 'lodash';
-import { isEth, isWeth, replaceEthWithWeth, replaceWethWithEth } from '~/lib/services/token/token-util';
+import { isEth, replaceEthWithWeth, replaceWethWithEth } from '~/lib/services/token/token-util';
 import { useUserAccount } from '~/lib/user/useUserAccount';
 import { usePool } from '~/modules/pool/lib/usePool';
 import { useGetTokens } from '~/lib/global/useToken';
-import { TokenAmountHumanReadable } from '~/lib/services/token/token-types';
+
+function calculateBptOut(bptTotalSupply: string, aI: string, b: string) {
+    return parseFloat(bptTotalSupply) * (parseFloat(aI || '') / parseFloat(b));
+}
 
 export function usePoolJoinGetProportionalInvestmentAmount() {
     const { poolService, pool } = usePool();
@@ -13,116 +16,79 @@ export function usePoolJoinGetProportionalInvestmentAmount() {
     const { userAddress } = useUserAccount();
     const { priceForAmount } = useGetTokens();
 
-    const totalValueArray: any[] = [];
-    const smallestValueArray: any[] = [];
-    userInvestTokenBalances.forEach((balance) => {
-        const investOption = pool.investConfig.options.find((option) => {
-            const tokenOption = option.tokenOptions.find(
-                (tokenOption) => tokenOption.address === replaceEthWithWeth(balance.address),
-            );
-
-            return !!tokenOption;
-        });
-
-        const poolToken = investOption ? pool.tokens[investOption.poolTokenIndex] : undefined;
-
-        if (pool.__typename === 'GqlPoolWeighted') {
-            if (poolToken?.__typename === 'GqlPoolTokenPhantomStable') {
-                const totalLiquidity = poolToken.pool.totalLiquidity;
-                const totalValue = sumBy(
-                    pool.investConfig.options
-                        .map((option, index) =>
-                            option.poolTokenIndex === poolToken.index ? userInvestTokenBalances[index] : [],
-                        )
-                        .flat(),
-                    priceForAmount,
-                );
-
-                const token = poolToken.pool.tokens
-                    .filter((token) =>
-                        'pool' in token
-                            ? token.pool.tokens.find((nestedToken) => nestedToken.address === balance.address)
-                            : null,
-                    )
-                    .flat()[0];
-                const weight =
-                    priceForAmount({ address: token.address, amount: token?.totalBalance }) /
-                    parseFloat(totalLiquidity);
-                const weightedValue = weight * totalValue;
-                const tokenValue = priceForAmount(balance);
-                smallestValueArray.push({
-                    ...balance,
-                    poolIndex: poolToken.index,
-                    ratioDiff: (tokenValue / weightedValue) * tokenValue,
-                    weight,
-                });
-                totalValueArray.push({ poolIndex: poolToken?.index, amount: weightedValue, weight: poolToken?.weight });
-            } else {
-                totalValueArray.push({
-                    poolIndex: poolToken?.index,
-                    address: balance.address,
-                    amount: balance.amount,
-                    weight: poolToken?.weight,
-                });
-            }
-        }
-    });
-
-    const totalValue = sum(
+    const smallestValue = sortBy(
         pool.tokens.map((token) => {
-            const values = totalValueArray.filter((item) => item.poolIndex === token.index);
-            if (values.length === 1) {
-                return priceForAmount({ address: values[0].address, amount: values[0].amount });
+            const userInvestTokenBalanceAddresses = userInvestTokenBalances.map((balance) => balance.address);
+
+            const bptTotalSupply =
+                token.__typename === 'GqlPoolTokenPhantomStable'
+                    ? token.pool.totalShares
+                    : pool.dynamicData.totalShares;
+            const b = token.balance;
+
+            if (token.__typename === 'GqlPoolTokenPhantomStable') {
+                const nestedTokens = token.pool.tokens.map((poolToken) => {
+                    const tokenAddress =
+                        poolToken.__typename === 'GqlPoolTokenLinear' &&
+                        poolToken.pool.tokens.find((nestedPoolToken) =>
+                            userInvestTokenBalanceAddresses.includes(nestedPoolToken.address),
+                        )?.address;
+                    const b = poolToken.balance;
+                    const aI =
+                        userInvestTokenBalances.find((balance) => balance.address === tokenAddress)?.amount || '';
+                    const bptOut = calculateBptOut(bptTotalSupply, aI, b);
+                    return { address: tokenAddress, amount: aI, bptOut };
+                });
+                const smallestValueTokenArray = sortBy(nestedTokens, 'bptOut');
+                const smallestValueToken = smallestValueTokenArray[0];
+
+                const aI = smallestValueToken.bptOut.toString();
+                const bptOut = calculateBptOut(pool.dynamicData.totalShares, aI, b);
+
+                const totalValue =
+                    sum(
+                        smallestValueTokenArray
+                            .slice(1)
+                            .map(
+                                (nestedToken) =>
+                                    (1 / (nestedToken.bptOut / smallestValueToken.bptOut)) *
+                                    parseFloat(nestedToken.amount || ''),
+                            ),
+                    ) + parseFloat(smallestValueToken.amount || '');
+
+                return {
+                    bptOut,
+                    smallest: {
+                        ...smallestValueToken,
+                        amount: totalValue.toString(),
+                    },
+                };
             } else {
-                return sum(values.map((value) => value.amount));
+                const tokenAddress =
+                    token.__typename === 'GqlPoolTokenLinear'
+                        ? token.pool.tokens.find((poolToken) =>
+                              userInvestTokenBalanceAddresses.includes(poolToken.address),
+                          )?.address
+                        : token.address;
+                const aI = userInvestTokenBalances.find((balance) => balance.address === tokenAddress)?.amount || '';
+                const bptOut = calculateBptOut(bptTotalSupply, aI, b);
+
+                return {
+                    bptOut,
+                    smallest: {
+                        address: tokenAddress,
+                        amount: aI,
+                    },
+                };
             }
         }),
-    );
+        'bptOut',
+    )[0];
 
-    totalValueArray.forEach((item) => {
-        if ('address' in item) {
-            const tokenValue = priceForAmount({ address: item.address, amount: item.amount });
-            const weightedValue = item.weight * totalValue;
-            smallestValueArray.push({
-                address: item.address,
-                amount: item.amount,
-                poolIndex: item.poolIndex,
-                ratioDiff: (tokenValue / weightedValue) * tokenValue,
-            });
-        }
-    });
-
-    const phantomStableIndices = pool.tokens
-        .filter((token) => token.__typename === 'GqlPoolTokenPhantomStable')
-        .map((token) => token.index);
-
-    if (phantomStableIndices.length > 0) {
-        phantomStableIndices.forEach((idx) => {
-            const filteredSmallestValueArray = smallestValueArray.filter((item) => item.poolIndex === idx);
-            const psSmallestValue = sortBy(filteredSmallestValueArray, 'ratioDiff')[0];
-
-            const totalValue =
-                sum(
-                    filteredSmallestValueArray
-                        .filter((item) => item.address !== psSmallestValue.address)
-                        .map(
-                            (item) => parseFloat(item.amount) * (1 / parseFloat(psSmallestValue.weight)) * item.weight,
-                        ),
-                ) + parseFloat(psSmallestValue.amount);
-            const updatedArray = filteredSmallestValueArray.map((item) => ({
-                ...item,
-                amount: totalValue.toString(),
-            }));
-            smallestValueArray.forEach((item, index) => {
-                const [update] = updatedArray.filter((update) => update.address === item.address);
-                if (update) {
-                    smallestValueArray[index] = update;
-                }
-            });
-        });
-    }
-
-    const tokenWithSmallestValue = sortBy(smallestValueArray, 'ratioDiff')[0];
+    const tokenWithSmallestValue = {
+        address: smallestValue.smallest.address || '',
+        amount: smallestValue.smallest.amount || '',
+    };
 
     const query = useQuery(
         [
@@ -137,7 +103,7 @@ export function usePoolJoinGetProportionalInvestmentAmount() {
             const hasEth = !!userInvestTokenBalances.find((token) => isEth(token.address));
             const fixedAmount = {
                 ...tokenWithSmallestValue,
-                address: replaceEthWithWeth(tokenWithSmallestValue.address),
+                address: replaceEthWithWeth(tokenWithSmallestValue.address || ''),
             };
 
             if (!poolService.joinGetProportionalSuggestionForFixedAmount) {
@@ -153,7 +119,6 @@ export function usePoolJoinGetProportionalInvestmentAmount() {
                 tokenProportionalAmounts: Object.fromEntries(
                     result.map((item) => [hasEth ? replaceWethWithEth(item.address) : item.address, item.amount]),
                 ),
-                // this is still not ideal for when there are multiple invest options for a token
                 totalValueProportionalAmounts: sumBy(result, priceForAmount),
             };
         },
