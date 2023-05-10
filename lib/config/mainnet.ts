153d1b62cc25003cdb4059f3b1dbd37fd8169c2f
groninge <groninge@gmail.com>
2023-05-10 13:51:16 +0200

    set default swap tokens

--- lib/config/mainnet.ts
+++ lib/config/mainnet.ts
@@ -65,8 +65,8 @@ export const mainnetNetworkConfig: NetworkConfig = {
     },
     beetsPoolOwnerAddress: '0xcd983793adb846dce4830c22f30c7ef0c864a776',
     masterChefContractAddress: '0x8166994d9ebBe5829EC86Bd81258149B87faCfd3',
-    defaultTokenIn: '0x04068da6c83afcfa0e13ba15a6696662335d5b75',
-    defaultTokenOut: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
+    defaultTokenIn: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    defaultTokenOut: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     farmTypeName: 'farm',
     additionalLinks: [
         {
