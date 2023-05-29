41792f67982c2c4effb6be6db592f2f4b63b0c5b
groninge <groninge@gmail.com>
2023-05-29 09:17:33 +0200

    clear invest state to prevent unproportional invest

--- modules/pool/invest/PoolInvestModal.tsx
+++ modules/pool/invest/PoolInvestModal.tsx
@@ -57,6 +57,7 @@ export function PoolInvestModal({
     const containerControls = useAnimation();
     const modalContainerRef = useRef<HTMLDivElement | null>(null);
     const lastModalBounds = useRef<DOMRect | null>(null);
+    const { clearInvestState } = useInvestState();
 
     function onModalClose() {
         if (investComplete) {
@@ -213,6 +214,8 @@ export function PoolInvestModal({
                                         onShowProportional={() => {
                                             setInvestType('proportional');
                                             setModalState('proportional');
+                                            // clear invest state here to prevent accidental unproportional invest
+                                            clearInvestState();
                                         }}
                                         onShowCustom={() => {
                                             setInvestType('custom');
