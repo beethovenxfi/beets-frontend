730b670af52a4c8da4effaf0fdad097a2d0a1244
groninge <groninge@gmail.com>
2023-05-31 16:14:53 +0200

    Revert "unused hook?"
    This reverts commit e38712bfa20679d106944608619d38c78b93c79b.

--- modules/pool/invest/lib/usePoolJoinGetProportionalSuggestionForFixedAmount.ts
+++ modules/pool/invest/lib/usePoolJoinGetProportionalSuggestionForFixedAmount.ts
@@ -1,62 +1,62 @@
-// import { useReactiveVar } from '@apollo/client';
-// import { investStateVar } from '~/modules/pool/invest/lib/useInvestState';
-// import {
-//     isEth,
-//     replaceEthWithWeth,
-//     replaceWethWithEth,
-//     tokenAmountsGetArrayFromMap,
-// } from '~/lib/services/token/token-util';
-// import { useQuery } from 'react-query';
-// import { AmountHumanReadableMap } from '~/lib/services/token/token-types';
-// import { useInvest } from '~/modules/pool/invest/lib/useInvest';
-// import { usePool } from '~/modules/pool/lib/usePool';
+import { useReactiveVar } from '@apollo/client';
+import { investStateVar } from '~/modules/pool/invest/lib/useInvestState';
+import {
+    isEth,
+    replaceEthWithWeth,
+    replaceWethWithEth,
+    tokenAmountsGetArrayFromMap,
+} from '~/lib/services/token/token-util';
+import { useQuery } from 'react-query';
+import { AmountHumanReadableMap } from '~/lib/services/token/token-types';
+import { useInvest } from '~/modules/pool/invest/lib/useInvest';
+import { usePool } from '~/modules/pool/lib/usePool';
 
-// export function usePoolJoinGetProportionalSuggestionForFixedAmount() {
-//     const { poolService } = usePool();
-//     const { inputAmounts } = useReactiveVar(investStateVar);
-//     const tokenAmountsIn = tokenAmountsGetArrayFromMap(inputAmounts);
-//     const { selectedInvestTokens } = useInvest();
+export function usePoolJoinGetProportionalSuggestionForFixedAmount() {
+    const { poolService } = usePool();
+    const { inputAmounts } = useReactiveVar(investStateVar);
+    const tokenAmountsIn = tokenAmountsGetArrayFromMap(inputAmounts);
+    const { selectedInvestTokens } = useInvest();
 
-//     return useQuery(
-//         [{ key: 'joinGetProportionalSuggestionForFixedAmount', tokenAmountsIn }],
-//         async ({ queryKey }) => {
-//             const hasEth = !!selectedInvestTokens.find((token) => isEth(token.address));
-//             const fixedAmount = tokenAmountsIn[0];
+    return useQuery(
+        [{ key: 'joinGetProportionalSuggestionForFixedAmount', tokenAmountsIn }],
+        async ({ queryKey }) => {
+            const hasEth = !!selectedInvestTokens.find((token) => isEth(token.address));
+            const fixedAmount = tokenAmountsIn[0];
 
-//             if (!poolService.joinGetProportionalSuggestionForFixedAmount) {
-//                 return {};
-//             }
+            if (!poolService.joinGetProportionalSuggestionForFixedAmount) {
+                return {};
+            }
 
-//             const result = await poolService.joinGetProportionalSuggestionForFixedAmount(
-//                 fixedAmount,
-//                 selectedInvestTokens.map((token) => replaceEthWithWeth(token.address)),
-//             );
-//             const proportionalSuggestion = Object.fromEntries(
-//                 result.map((item) => [hasEth ? replaceWethWithEth(item.address) : item.address, item.amount]),
-//             );
+            const result = await poolService.joinGetProportionalSuggestionForFixedAmount(
+                fixedAmount,
+                selectedInvestTokens.map((token) => replaceEthWithWeth(token.address)),
+            );
+            const proportionalSuggestion = Object.fromEntries(
+                result.map((item) => [hasEth ? replaceWethWithEth(item.address) : item.address, item.amount]),
+            );
 
-//             return isProportionalSuggestionValid(inputAmounts, proportionalSuggestion) ? proportionalSuggestion : {};
-//         },
-//         { enabled: tokenAmountsIn.length > 0, staleTime: 0, cacheTime: 0 },
-//     );
-// }
+            return isProportionalSuggestionValid(inputAmounts, proportionalSuggestion) ? proportionalSuggestion : {};
+        },
+        { enabled: tokenAmountsIn.length > 0, staleTime: 0, cacheTime: 0 },
+    );
+}
 
-// function isProportionalSuggestionValid(
-//     inputAmounts: AmountHumanReadableMap,
-//     proportionalAmounts: AmountHumanReadableMap,
-// ) {
-//     if (Object.keys(proportionalAmounts).length === 0) {
-//         //there is no proportional suggestion
-//         return true;
-//     }
+function isProportionalSuggestionValid(
+    inputAmounts: AmountHumanReadableMap,
+    proportionalAmounts: AmountHumanReadableMap,
+) {
+    if (Object.keys(proportionalAmounts).length === 0) {
+        //there is no proportional suggestion
+        return true;
+    }
 
-//     const addresses = Object.keys(inputAmounts);
+    const addresses = Object.keys(inputAmounts);
 
-//     for (const address of addresses) {
-//         if (inputAmounts[address] !== '' && inputAmounts[address] !== proportionalAmounts[address]) {
-//             return false;
-//         }
-//     }
+    for (const address of addresses) {
+        if (inputAmounts[address] !== '' && inputAmounts[address] !== proportionalAmounts[address]) {
+            return false;
+        }
+    }
 
-//     return true;
-// }
+    return true;
+}
