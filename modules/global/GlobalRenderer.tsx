7d1a91f990d2cd65cdbb48253711c79e71230892
groninge <groninge@gmail.com>
2023-06-05 16:53:25 +0200

    add temp banner for op upgrade

--- modules/global/GlobalRenderer.tsx
+++ modules/global/GlobalRenderer.tsx
@@ -2,7 +2,9 @@ import { useUserAccount } from '~/lib/user/useUserAccount';
 import { useGetAppGlobalPollingDataQuery, useGetUserDataQuery } from '~/apollo/generated/graphql-codegen-generated';
 import { useEffectOnce } from '~/lib/util/custom-hooks';
 import { useEffect, useRef } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, HStack } from '@chakra-ui/react';
+import { ToastType, useToast } from '~/components/toast/BeetsToast';
+import { useNetworkConfig } from '~/lib/global/useNetworkConfig';
 
 export function GlobalRenderer() {
     const currentUserAddress = useRef('');
@@ -11,6 +13,8 @@ export function GlobalRenderer() {
     const { startPolling: startPollingUserData, refetch: refetchUserData } = useGetUserDataQuery({
         fetchPolicy: 'network-only',
     });
+    const { showToast } = useToast();
+    const networkConfig = useNetworkConfig();
 
     useEffectOnce(() => {
         startPollingAppData(30_000);
@@ -26,6 +30,23 @@ export function GlobalRenderer() {
             currentUserAddress.current = userAddress;
         }
     }, [isConnected, userAddress]);
+
+    useEffect(() => {
+        if (networkConfig.chainId === '10') {
+            showToast({
+                id: 'op-upgrade-alert',
+                type: ToastType.Warn,
+                content: (
+                    <HStack>
+                        <Box>
+                            From June 6th 16:00 UTC until June 6th 20:00 UTC (expected) Optimism will be rolling out its
+                            highly anticipated Bedrock upgrade.
+                        </Box>
+                    </HStack>
+                ),
+            });
+        }
+    }, []);
 
     return <Box>{null}</Box>;
 }
