fb146319f6e58acc8c519fede3fdba1390ed896c
Daniel <danielBeets@protonmail.com>
2023-06-12 17:47:32 +0800

    update badge text

--- components/pool-badge/lib/pool-badge-tooltips.ts
+++ components/pool-badge/lib/pool-badge-tooltips.ts
@@ -10,5 +10,6 @@ export const PoolBadgeTooltips: { [type: string]: string } = {
     reaper: 'This pool deposits a portion of its assets into the Reaper.Farm Multi-Strategy vault(s) to generate boosted yield. Assets are distributed across many protocols to generate the highest risk-adjusted yield.',
     'beefy-exactly':
         'This pool deposits a portion of its assets into Beefy vault(s) to generate boosted yield. The vault(s) are configured to deposit assets to Exactly in supply-only mode.',
-    gyroscope: 'This pool is built by Gyroscope. It is a very new and highly experimental concentrated liquidity pool.',
+    gyroscope:
+        'The Gyroscope E-CLP merges the UX of StableSwap with the capital efficiency of Uniswap V3, featuring asymmetric price bounds and custom liquidity profiles.',
 };
