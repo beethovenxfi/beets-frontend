0b9a08d72bb6dd914bd1abc990e12827a398a5da
groninge <groninge@gmail.com>
2023-07-03 07:48:24 +0200

    make responsive

--- modules/lge/create/LgeCreate.tsx
+++ modules/lge/create/LgeCreate.tsx
@@ -11,11 +11,13 @@ import {
     StepTitle,
     Stepper,
     useSteps,
+    useBreakpointValue,
 } from '@chakra-ui/react';
 import LgeCreateDetailsForm, { DetailsFormData } from './forms/LgeCreateDetailsForm';
 import LgeCreateConfigurationForm, { ConfigurationFormData } from './forms/LgeCreateConfigurationForm';
 import { useEffect, useState } from 'react';
 import LgeCreateCreation from './forms/LgeCreateCreation';
+import scrollIntoView from 'smooth-scroll-into-view-if-needed';
 
 const steps = [
     { title: 'Details', description: 'Enter your LBP details' },
@@ -28,9 +30,18 @@ export function LgeCreate() {
         index: 0,
         count: steps.length,
     });
+    const isMobile = useBreakpointValue({ base: true, lg: false });
 
     const [detailsFormData, setDetailsFormData] = useState<DetailsFormData>();
     const [configurationFormData, setConfigurationFormData] = useState<ConfigurationFormData>();
+
+    const element = document.getElementById('stepper');
+    if (element) {
+        scrollIntoView(element, {
+            behavior: 'smooth',
+            scrollMode: 'if-needed',
+        });
+    }
 
     // useEffect(() => {
     //     console.log({ detailsFormData, configurationFormData });
@@ -38,11 +49,18 @@ export function LgeCreate() {
 
     return (
         <>
-            <Heading as="h1" size="2xl" mb="16">
+            <Heading as="h1" size="2xl" mb={{ base: '8', lg: '16' }}>
                 Create an LBP
             </Heading>
-            <Box w="1024px">
-                <Stepper index={activeStep} mb="16">
+            <Box w={{ base: undefined, lg: '1024px' }}>
+                <Stepper
+                    id="stepper"
+                    index={activeStep}
+                    mb={{ base: '4', lg: '16' }}
+                    orientation={isMobile ? 'vertical' : undefined}
+                    height={{ base: '200px', lg: undefined }}
+                    gap={{ base: '0', lg: undefined }}
+                >
                     {steps.map((step, index) => (
                         <Step key={index}>
                             <StepIndicator>
