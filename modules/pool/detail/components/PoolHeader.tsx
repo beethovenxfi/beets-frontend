909dc66e89e914011d402fb695e16ab89ad6bd74
groninge <groninge@gmail.com>
2023-08-02 11:29:30 +0200

    add helper function

--- modules/pool/detail/components/PoolHeader.tsx
+++ modules/pool/detail/components/PoolHeader.tsx
@@ -10,6 +10,7 @@ import {
     Wrap,
     WrapItem,
     Link,
+    StackProps,
 } from '@chakra-ui/react';
 import numeral from 'numeral';
 import { PoolTokenPill } from '~/components/token/PoolTokenPill';
@@ -21,11 +22,45 @@ import { AddressZero } from '@ethersproject/constants';
 import { usePool } from '~/modules/pool/lib/usePool';
 import AuraLogo from '~/assets/logo/aura_iso_colors.png';
 
+interface Props {
+    trigger: React.ReactNode;
+    content: React.ReactNode;
+}
+
+function CustomTooltip({ trigger, content, ...rest }: Props & StackProps) {
+    // temp fix: https://github.com/chakra-ui/chakra-ui/issues/5896#issuecomment-1104085557
+    const PopoverTrigger: React.FC<{ children: React.ReactNode }> = OrigPopoverTrigger;
+
+    return (
+        <Popover trigger="hover" placement="right">
+            <PopoverTrigger>
+                <HStack
+                    paddingX="3"
+                    paddingY="2"
+                    bg="whiteAlpha.200"
+                    spacing="2"
+                    fontSize="md"
+                    rounded="full"
+                    color="beets.base.50"
+                    justifyContent="center"
+                    fontWeight="semibold"
+                    {...rest}
+                >
+                    {trigger}
+                </HStack>
+            </PopoverTrigger>
+            <PopoverContent w="200px" bgColor="beets.base.800" shadow="2xl">
+                <Box p="2" fontSize="sm" bgColor="whiteAlpha.200">
+                    {content}
+                </Box>
+            </PopoverContent>
+        </Popover>
+    );
+}
+
 function PoolHeader() {
     const networkConfig = useNetworkConfig();
     const { pool } = usePool();
-    // temp fix: https://github.com/chakra-ui/chakra-ui/issues/5896#issuecomment-1104085557
-    const PopoverTrigger: React.FC<{ children: React.ReactNode }> = OrigPopoverTrigger;
 
     const hasBeetsOwner = pool.owner === networkConfig.beetsPoolOwnerAddress;
     const hasZeroOwner = pool.owner === AddressZero;
@@ -55,19 +90,9 @@ function PoolHeader() {
                 ))}
             </Wrap>
             <HStack>
-                <Popover trigger="hover" placement="right">
-                    <PopoverTrigger>
-                        <HStack
-                            paddingX="3"
-                            paddingY="2"
-                            bg="whiteAlpha.200"
-                            spacing="2"
-                            fontSize="md"
-                            rounded="full"
-                            color="beets.base.50"
-                            justifyContent="center"
-                            fontWeight="semibold"
-                        >
+                <CustomTooltip
+                    trigger={
+                        <>
                             {!hasZeroOwner && (
                                 <Flex alignItems="center">
                                     {hasBeetsOwner ? (
@@ -81,52 +106,35 @@ function PoolHeader() {
                                 <Text>{numeral(pool.dynamicData.swapFee).format('0.0[00]%')}</Text>
                                 <Text>{swapFeeType} Fee</Text>
                             </HStack>
-                        </HStack>
-                    </PopoverTrigger>
-                    <PopoverContent w="200px" bgColor="beets.base.800" shadow="2xl">
-                        <Box p="2" fontSize="sm" bgColor="whiteAlpha.200">
-                            {tooltipText1} {!hasZeroOwner && tooltipText2}
-                        </Box>
-                    </PopoverContent>
-                </Popover>
+                        </>
+                    }
+                    content={`${tooltipText1} ${!hasZeroOwner && tooltipText2}`}
+                />
                 {hasAuraStaking && (
-                    <HStack
-                        paddingX="3"
-                        paddingY="2"
-                        bg="whiteAlpha.300"
-                        spacing="2"
-                        fontSize="md"
-                        rounded="full"
-                        justifyContent="center"
-                        fontWeight="semibold"
+                    <CustomTooltip
+                        trigger={
+                            <Link
+                                mr="2"
+                                href={networkConfig.auraStaking[pool.id]}
+                                isExternal
+                                style={{ textDecoration: 'none' }}
+                            >
+                                <HStack>
+                                    <Text mr="2" color="beets.base.50">
+                                        Boosted rewards are available on Aura
+                                    </Text>
+                                    <Image src={AuraLogo} alt="Aura Finance" height="24px" width="24px" />
+                                </HStack>
+                            </Link>
+                        }
+                        content={
+                            'For this pool you can deposit & stake your BPT on Aura Finance for extra boosted rewards'
+                        }
                         border="2px"
                         borderColor="aura.pink"
                         _hover={{ borderColor: 'aura.purple' }}
-                    >
-                        <Popover trigger="hover" placement="right">
-                            <PopoverTrigger>
-                                <Link
-                                    mr="2"
-                                    href={networkConfig.auraStaking[pool.id]}
-                                    isExternal
-                                    style={{ textDecoration: 'none' }}
-                                >
-                                    <HStack>
-                                        <Text mr="2" color="beets.base.50">
-                                            Boosted rewards are available on Aura
-                                        </Text>
-                                        <Image src={AuraLogo} alt="Aura Finance" height="24px" width="24px" />
-                                    </HStack>
-                                </Link>
-                            </PopoverTrigger>
-                            <PopoverContent w="200px" bgColor="beets.base.800" shadow="2xl" ml="6">
-                                <Box p="2" fontSize="sm" bgColor="whiteAlpha.200">
-                                    For this pool you can deposit & stake your BPT on Aura Finance for extra boosted
-                                    rewards
-                                </Box>
-                            </PopoverContent>
-                        </Popover>
-                    </HStack>
+                        bg="whiteAlpha.300"
+                    />
                 )}
             </HStack>
         </VStack>
