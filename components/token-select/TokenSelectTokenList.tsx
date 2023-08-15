16ba28fc3448d5cbc05bbdf5f0873a784a3d5e79
1saf <me@ansaf.net>
2023-08-15 23:04:19 +1000

    fix: crash on null

--- components/token-select/TokenSelectTokenList.tsx
+++ components/token-select/TokenSelectTokenList.tsx
@@ -51,7 +51,7 @@ export function TokenSelectTokenList({ listHeight, searchTerm, onTokenRowClick, 
             itemSize={56}
             renderItem={({ index, style }) => {
                 const token = propFilteredTokens[index];
-                const userBalance = tokenFindTokenAmountForAddress(token.address, userBalances);
+                const userBalance = tokenFindTokenAmountForAddress(token?.address || '', userBalances);
                 return (
                     <div style={style} key={index}>
                         <TokenRow
