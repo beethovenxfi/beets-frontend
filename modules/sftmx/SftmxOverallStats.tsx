03b2c8f387ec11f38e1150e4a11d9fac42987e76
groninge <groninge@gmail.com>
2023-12-28 09:19:55 +0100

    format layout

--- modules/sftmx/SftmxOverallStats.tsx
+++ modules/sftmx/SftmxOverallStats.tsx
@@ -25,56 +25,60 @@ export default function SftmxOverallStats() {
             </Box>
             <Card p={{ base: '4', lg: '8' }} h="full" w="full">
                 <VStack spacing="8" w="full" align="flex-start">
-                    <VStack spacing="2" align="flex-start">
-                        <Heading size="sm">Staking APR</Heading>
-                        <HStack>
-                            <div className="apr-stripes">
-                                {numeral(data?.sftmxGetStakingData.stakingApr || '').format('0.00%')}
-                            </div>
-                        </HStack>
-                    </VStack>
-                    <Divider />
-                    <VStack w="full" align="flex-start">
-                        <Heading size="sm">FTM</Heading>
-                        <HStack w="full" justifyContent="space-between">
-                            <Box>Total amount</Box>
-                            <VStack spacing="0" align="flex-end">
+                    <Card w="full" p="4">
+                        <VStack w="full" align="flex-start">
+                            <Heading size="sm">Staking</Heading>
+                            <HStack w="full" justifyContent="space-between">
+                                <Box>APR</Box>
                                 <Text color="white">
-                                    {numeral(totalFtmAmount || '').format('0,0.00a')}
-                                    <Text as="span">&nbsp;FTM </Text>
+                                    {numeral(data?.sftmxGetStakingData.stakingApr || '').format('0.00%')}
                                 </Text>
-                                <Text fontSize="sm" color="gray.200">
-                                    ({numberFormatUSDValue(totalFtmAmountUsd)})
-                                </Text>
-                            </VStack>
-                        </HStack>
-                        <Divider my="2" />
-                        <HStack w="full" justifyContent="space-between">
-                            <Box>Total amount staked</Box>
-                            <VStack spacing="0" align="flex-end">
-                                <Text color="white">
-                                    {numeral(data?.sftmxGetStakingData.totalFtmAmountStaked).format('0,0.00a')}
-                                    <Text as="span">&nbsp;FTM</Text>
-                                </Text>
-                                <Text fontSize="sm" color="gray.200">
-                                    ({numberFormatUSDValue(totalFtmAmountStakedUsd)})
-                                </Text>
-                            </VStack>
-                        </HStack>
-                        <Divider my="2" />
-                        <HStack w="full" justifyContent="space-between">
-                            <Box>Total amount free</Box>
-                            <VStack spacing="0" align="flex-end">
-                                <Text color="white">
-                                    {numeral(data?.sftmxGetStakingData.totalFtmAmountInPool).format('0,0.00a')}
-                                    <Text as="span">&nbsp;FTM</Text>
-                                </Text>
-                                <Text fontSize="sm" color="gray.200">
-                                    ({numberFormatUSDValue(totalFtmAmountInPoolUsd)})
-                                </Text>
-                            </VStack>
-                        </HStack>
-                    </VStack>
+                            </HStack>
+                        </VStack>
+                    </Card>
+                    <Card w="full" p="4">
+                        <VStack w="full" align="flex-start">
+                            <Heading size="sm">FTM</Heading>
+                            <HStack w="full" justifyContent="space-between">
+                                <Box>Total amount</Box>
+                                <VStack spacing="0" align="flex-end">
+                                    <Text color="white">
+                                        {numeral(totalFtmAmount || '').format('0,0.00a')}
+                                        <Text as="span">&nbsp;FTM </Text>
+                                    </Text>
+                                    <Text fontSize="sm" color="gray.200">
+                                        ({numberFormatUSDValue(totalFtmAmountUsd)})
+                                    </Text>
+                                </VStack>
+                            </HStack>
+                            <Divider my="2" />
+                            <HStack w="full" justifyContent="space-between">
+                                <Box>Total amount staked</Box>
+                                <VStack spacing="0" align="flex-end">
+                                    <Text color="white">
+                                        {numeral(data?.sftmxGetStakingData.totalFtmAmountStaked).format('0,0.00a')}
+                                        <Text as="span">&nbsp;FTM</Text>
+                                    </Text>
+                                    <Text fontSize="sm" color="gray.200">
+                                        ({numberFormatUSDValue(totalFtmAmountStakedUsd)})
+                                    </Text>
+                                </VStack>
+                            </HStack>
+                            <Divider my="2" />
+                            <HStack w="full" justifyContent="space-between">
+                                <Box>Total amount free</Box>
+                                <VStack spacing="0" align="flex-end">
+                                    <Text color="white">
+                                        {numeral(data?.sftmxGetStakingData.totalFtmAmountInPool).format('0,0.00a')}
+                                        <Text as="span">&nbsp;FTM</Text>
+                                    </Text>
+                                    <Text fontSize="sm" color="gray.200">
+                                        ({numberFormatUSDValue(totalFtmAmountInPoolUsd)})
+                                    </Text>
+                                </VStack>
+                            </HStack>
+                        </VStack>
+                    </Card>
                 </VStack>
             </Card>
         </VStack>
