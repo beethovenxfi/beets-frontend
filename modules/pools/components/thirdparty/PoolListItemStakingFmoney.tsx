0d3c8419580fe6d3388811974d7c9fb28db296c3
groninge <groninge@gmail.com>
2024-03-15 09:21:12 +0100

    apply icon shadow

--- modules/pools/components/thirdparty/PoolListItemStakingFmoney.tsx
+++ modules/pools/components/thirdparty/PoolListItemStakingFmoney.tsx
@@ -7,7 +7,7 @@ export function PoolListItemStakingFmoney() {
     return (
         <BeetsTooltip label="For this pool you can deposit & stake your BPT on fMoney Markets for rewards" noImage>
             <Box ml="2" mt="1">
-                <Image src={FmoneyLogo} alt="fMoney Markets" height="24px" width="24px" />
+                <Image src={FmoneyLogo} alt="fMoney Markets" height="24px" width="24px" className="icon-shadow" />
             </Box>
         </BeetsTooltip>
     );
