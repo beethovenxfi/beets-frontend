60b00e1689cb17b8252a3a782783f22a26f1b1cb
groninge <groninge@gmail.com>
2024-03-19 08:26:36 +0100

    remove in pool list

--- modules/pools/components/PoolListItemStaking.tsx
+++ modules/pools/components/PoolListItemStaking.tsx
@@ -1,7 +1,6 @@
 import { networkConfig } from '~/lib/config/network-config';
 import { PoolListItemStakingAura } from './thirdparty/PoolListItemStakingAura';
 import { PoolListItemStakingFmoney } from './thirdparty/PoolListItemStakingFmoney';
-import { PoolListItemInvestingGyro } from './thirdparty/PoolListItemInvestingGyro';
 
 export function PoolListItemStaking({ poolId }: { poolId: string }) {
     const name = networkConfig.thirdPartyStakingPools.find((pool) => pool.poolId === poolId)?.name;
@@ -11,8 +10,6 @@ export function PoolListItemStaking({ poolId }: { poolId: string }) {
             return <PoolListItemStakingAura />;
         case 'fmoney':
             return <PoolListItemStakingFmoney />;
-        case 'gyro':
-            return <PoolListItemInvestingGyro />;
         default:
             return null;
     }
