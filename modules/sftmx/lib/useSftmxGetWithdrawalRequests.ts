b782390c1449454eaf9d90211ff4c40e0bff2bad
groninge <groninge@gmail.com>
2024-03-20 16:18:59 +0100

    if no wallet send zero address

--- modules/sftmx/lib/useSftmxGetWithdrawalRequests.ts
+++ modules/sftmx/lib/useSftmxGetWithdrawalRequests.ts
@@ -1,26 +1,13 @@
-import { useQuery } from 'react-query';
 import { useSftmxGetWithdrawalRequestsQuery } from '~/apollo/generated/graphql-codegen-generated';
 import { useUserAccount } from '~/lib/user/useUserAccount';
+import { AddressZero } from '@ethersproject/constants';
 
 export function useSftmxGetWithdrawalRequests() {
     const { userAddress } = useUserAccount();
 
-    const query = useQuery(
-        ['sftmxGetWithdrawalRequests', userAddress],
-        async () => {
-            const { data, ...rest } = useSftmxGetWithdrawalRequestsQuery({
-                variables: { user: userAddress as string },
-            });
+    const { data, ...rest } = useSftmxGetWithdrawalRequestsQuery({
+        variables: { user: (userAddress || AddressZero) as string },
+    });
 
-            return { data, ...rest };
-        },
-        { enabled: !!userAddress },
-    );
-
-    return {
-        ...query,
-        data: query.data?.data?.sftmxGetWithdrawalRequests || [],
-        startPolling: query.data?.startPolling,
-        stopPolling: query.data?.stopPolling,
-    };
+    return { data, ...rest };
 }
