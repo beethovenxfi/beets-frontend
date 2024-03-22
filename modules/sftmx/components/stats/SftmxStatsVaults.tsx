05ecc4d571aff5bf72d1fd6c78ec95ac51ade458
groninge <groninge@gmail.com>
2024-03-22 08:46:30 +0100

    sort on unlock time

--- modules/sftmx/components/stats/SftmxStatsVaults.tsx
+++ modules/sftmx/components/stats/SftmxStatsVaults.tsx
@@ -3,17 +3,20 @@ import { GqlSftmxStakingData } from '~/apollo/generated/graphql-codegen-generate
 import { PaginatedTable } from '~/components/table/PaginatedTable';
 import SftmxStatsVaultsHeader from './SftmxStatsVaultsHeader';
 import SftmxStatsVaultsRow from './SftmxStatsVaultsRow';
+import { sortBy } from 'lodash';
 
 interface Props {
     data: GqlSftmxStakingData;
 }
 
 export function SftmxStatsVaults({ data }: Props) {
+    const vaults = sortBy(data.vaults, 'unlockTimestamp');
+
     return (
         <Box mt="4">
             <PaginatedTable
                 width="full"
-                items={data.vaults}
+                items={vaults}
                 loading={false}
                 renderTableHeader={() => <SftmxStatsVaultsHeader />}
                 renderTableRow={(item, index) => <SftmxStatsVaultsRow vault={item} />}
