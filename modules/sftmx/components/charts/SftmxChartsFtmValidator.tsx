9fc0b3a4a3ff79230644a5646aa627c711bbd029
groninge <groninge@gmail.com>
2024-04-24 14:28:04 +0200

    fix type

--- modules/sftmx/components/charts/SftmxChartsFtmValidator.tsx
+++ modules/sftmx/components/charts/SftmxChartsFtmValidator.tsx
@@ -5,14 +5,11 @@ import { useBreakpointValue } from '@chakra-ui/react';
 import numeral from 'numeral';
 
 interface DataProps {
-    timestamp: number;
-    exchangeRate: string;
-    totalFtmAmount: string;
-    totalFtmAmountInPool: string;
-    totalFtmAmountStaked: string;
+    value: number;
+    name: string;
 }
 interface Props {
-    data: any[];
+    data: DataProps[];
 }
 
 export function SftmxChartsFtmValidator({ data }: Props) {
