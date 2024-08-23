e5620eea1c7dcfac15f8b5dddd86b55e371d611d
groninge <groninge@gmail.com>
2024-08-23 09:55:41 +0200

    fix merkl logo

--- modules/pools/components/thirdparty/PoolListItemStakingMerkl.tsx
+++ modules/pools/components/thirdparty/PoolListItemStakingMerkl.tsx
@@ -1,13 +1,13 @@
 import { Box } from '@chakra-ui/react';
 import BeetsTooltip from '~/components/tooltip/BeetsTooltip';
 import Image from 'next/image';
-import MerklLogo from '~/assets/logo/merkl-32x32.png';
+import MerklLogo from '~/assets/logo/merkl2x.png';
 
 export function PoolListItemStakingMerkl() {
     return (
         <BeetsTooltip label="For this pool you can earn rewards on Merkl" noImage>
             <Box ml="2" mt="1">
-                <Image src={MerklLogo} alt="Merkl" height="24px" width="24px" />
+                <Image src={MerklLogo} alt="Merkl" height="24px" width="24px" className="icon-shadow" />
             </Box>
         </BeetsTooltip>
     );
